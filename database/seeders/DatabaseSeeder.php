8566ba60cb4a8f493b73ffb0ade0fe667d2a938b
Kerim Nezo <kerim.nezo@gmail.com>
2024-07-05 17:04:37 +0200

    Added roles and permissions to database seeder
    Added roles and permission to agents and admin when created by seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,12 +18,33 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'admin',
-        //     'email' => 'kerim.nezo@gmail.com',
-        //     'phone_number' => '+387 61 034 357',
-        //     'password' => Hash::make('judaspriest'),
-        // ]);
+        //trebaš rijesiti problem nakon što obrišeš usere, ne briše se njihov zapis u model_has_roles
+        //još uvijek ima njihova veza sa modelom
+        //User::truncate(); - ovo briše sve zapise iz users tabele
+
+        $adminPermissions = [
+            'create agent',
+            'edit agent',
+            'delete agent',
+            'edit property',
+            'delete property',
+        ];
+
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        foreach ($adminPermissions as $permission)
+        {
+            $createdPermission = Permission::firstOrCreate(['name' => $permission]);
+            $adminRole->givePermissionTo($createdPermission);
+        }
+
+        $admin = User::factory()->create([
+            'name' => 'admin',
+            'email' => 'kerim.nezo@gmail.com',
+            'phone_number' => '+387 61 034 357',
+            'password' => Hash::make('judaspriest'),
+        ]);
+
+        $admin->assignRole($adminRole);
 
         $this->call(UserSeeder::class);
 
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,14 +4,34 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seeder koji stvara agent permisije i rolu, the kreira 4 agenta i assigna im Agent role.
      */
     public function run(): void
     {
-        User::factory(4)->create();
+        $agentPermissions = [
+            'create property',
+            'edit property',
+            'delete property',
+            'edit agent',
+        ];
+
+        $agentRole = Role::firstOrCreate(['name' => 'agent']);
+        foreach ($agentPermissions as $permission)
+        {
+            $createdPermission = Permission::firstOrCreate(['name' => $permission]);
+            $agentRole->givePermissionTo($createdPermission);
+        }
+
+        for ($x = 0; $x < 4; $x++)
+        {
+            $user = User::factory()->create();
+            $user->assignRole($agentRole);
+        }
     }
 }
