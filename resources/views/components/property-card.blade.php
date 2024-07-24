1615b604558a30416d2a01ee9d331d25e9bb2c72
Kerim Nezo <kerim.nezo@gmail.com>
2024-07-24 16:05:28 +0200

    Edited property blade and welcome page css
    As the summary says

--- resources/views/components/property-card.blade.php
+++ resources/views/components/property-card.blade.php
@@ -1,15 +1,16 @@
 @props(['property'])
 
-<div class="w-full h-[350px] text-white rounded-[15px] flex flex-col justify-end bg-cover" style="background-image:linear-gradient(to bottom, transparent, rgba(31, 31, 31, 0.73)), url('{{ $property->getFirstMediaUrl("property-photos")}}')">
+<div class="group w-full h-[300px] text-white rounded-[15px] flex flex-col justify-end bg-cover" style="background-image:linear-gradient(to bottom, transparent, rgba(31, 31, 31, 0.73)), url('{{ $property->getFirstMediaUrl("property-photos")}}')">
     <x-property-offer :lease="$property->lease_duration" />
 
-    <div id="price" class="m-0 p-0">
-        <span>{{ $property->name}} - {{ $property->price ?? 'cijene nema' }}</span>
+    <div id="price" class="m-0 p-0 flex flex-col justify-start w-full ">
+        <span class="mr-auto px-6 font-bold">$ {{ $property->price}}</span>
+        <span class="mr-auto px-6">{{ $property->name}}</span>
     </div>
     <div class="px-4 py-2">
         <hr class="w-[100%] ">
     </div>
-    <div id="attributes" class="pb-4 text-base w-full flex item-start ml-4">
+    <div id="attributes" class="pb-4 text-base w-full flex item-start ml-6">
         ovdje idu ikone i info o njima
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,11 @@
         <div class="text-2xl pb-[54px] pt-[103px]" id="query-title">
             <h1 class="text-color">WELCOME TO OUR PAGE</h1>
         </div>
+
         <div class="text-5xl pb-[160px]" id="query-sub-title">
             <h1 class="text-color">One step closer to your dream home...</h1>
         </div>
+
         <div id="query-form-div" class="flex justify-center items-center" >
             <div id="query-form" class="bg-[#5eb1f0] w-[80%] h-[120px] rounded-[5px]">
                 <table class="w-full mb-auto h-full">
@@ -18,6 +20,7 @@
                             <td class="pb-[20px] pl-[30px] w-[220px] align-bottom">
                                 <div class="flex flex-col justify-end h-full">
                                     <span class="mb-[5px] w-[220px] text-left">Property type</span>
+
                                     <select id="" class="rounded-[5px] border-2 border-[#989898] border-solid h-[35px] w-[220px] pl-[10px]" name="exact[type-of-asset-id]">
                                         <option value="0" >Choose property type</option>
                                         <option value="1">Office</option>
@@ -30,13 +33,16 @@
                             <td class="pl-[26px] pb-5 w-[200px] align-bottom">
                                 <div class="flex justify-end flex-col h-full">
                                     <span class="text-left mb-[5px] w-[200px]">Price</span>
+
                                     <input type="text" placeholder="1200" class="rounded-[5px] border-2 border-[#989898] border-solid h-[35px] w-[200px]" name="min-price">
                                 </div>
                             </td>
 
                             <td class="mt-auto pb-[26px] h-[51px] w-[26px]">
                                 <div class="flex flex-col justify-end h-full">
-                                    <span>to</span>
+                                    <span>
+                                        to
+                                    </span>
                                 </div>
                             </td>
 
@@ -65,7 +71,7 @@
             <div class="text-center text-xl py-12">
                 <div class="px-[10%] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full"> <!-- ovdje sad treba editovati kako se pozicioniraju -->
                     @foreach ($properties as $property )
-                        <a href="/property/{{$property->id}}">
+                        <a href="{{ route('single-property', ['id' => $property->id]) }}">
                             <x-property-card :$property/>
                         </a>
                     @endforeach
@@ -82,21 +88,25 @@
 
     <div class="py-16 text-center text-sm text-black bg-white"  id="property-types">
         <h1>Types of property for sale/rent</h1>
+
         <span>Imamo prikaz ikona svakog propertya koji nudimo na stranici</span>
     </div>
 
     <div class="py-16 text-center text-sm text-black bg-white"  id="about-section">
         <h1>About us</h1>
+
         <span>Prikaz poruke i misije naše kompanije</span>
     </div>
 
     <div class="py-16 text-center text-sm text-black bg-white"  id="team-section">
         <h1>Our team</h1>
+
         <span>prikaz agenata</span>
     </div>
 
     <footer class="py-16 text-center text-sm text-black bg-white" id="contact-section">
         <h1>Footer</h1>
+
         <span>Footer stranice, sa svim linkovima, social media linkovima i policy stuff</span>
     </footer>
 </x-property>
