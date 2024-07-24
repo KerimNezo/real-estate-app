a6ff76bdd0014455610c085c1a4c87286e6ec246
Kerim Nezo <kerim.nezo@gmail.com>
2024-07-24 16:41:07 +0200

    Edited font on property card
    As the summary says

--- resources/views/components/property-card.blade.php
+++ resources/views/components/property-card.blade.php
@@ -1,11 +1,11 @@
 @props(['property'])
 
-<div class="group w-full h-[300px] text-white rounded-[15px] flex flex-col justify-end bg-cover" style="background-image:linear-gradient(to bottom, transparent, rgba(31, 31, 31, 0.73)), url('{{ $property->getFirstMediaUrl("property-photos")}}')">
+<div id="pozadina" class="group w-full h-[300px] text-white rounded-[15px] flex flex-col justify-end bg-cover " style="background-image:linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.73)), url('{{ $property->getFirstMediaUrl("property-photos")}}')">
     <x-property-offer :lease="$property->lease_duration" />
 
     <div id="price" class="m-0 p-0 flex flex-col justify-start w-full ">
-        <span class="mr-auto px-6 font-bold">$ {{ $property->price}}</span>
-        <span class="mr-auto px-6">{{ $property->name}}</span>
+        <span class="mr-auto px-6 text-lg font-bold">$ {{ $property->price}}</span>
+        <span class="mr-auto px-6 text-lg ">{{ $property->name}}</span>
     </div>
     <div class="px-4 py-2">
         <hr class="w-[100%] ">
