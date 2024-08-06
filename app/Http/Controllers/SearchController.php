e7020f4a41e8b5721f5bb8f5a529850f9453a6bb
Kerim Nezo <kerim.nezo@gmail.com>
2024-08-06 12:52:54 +0200

    Added logs to SearchController file
    As the summary says, also did set up the logging in the
    public/log/laravel.log file using single log channel

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Property;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SearchController extends Controller
 {
@@ -14,13 +15,16 @@ class SearchController extends Controller
     {
         $properties = Property::query()->with(['user', 'type']);
 
-        if (! is_null($asset_id = $request->query('type-of-asset-id'))) {
+        if (! is_null($asset_id = $request->query('type-of-asset-id')) && $asset_id > 0) {
+            Log::info('Assets id is {asset-id}', ['asset-id' => $asset_id]);
             $properties = $properties->where('type_id', '=', $asset_id);
         }
         if (! is_null($min_price = $request->query('min-price'))) {
+            Log::info('Minimal price is {min-price}', ['min-price' => $min_price]);
             $properties = $properties->where('price', '>', $min_price);
         }
         if (! is_null($max_price = $request->query('max-price'))) {
+            Log::info('Maximal price is {max-price}', ['max-price' => $max_price]);
             $properties = $properties->where('price', '<', $max_price);
         }
 
