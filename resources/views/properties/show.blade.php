28d2730b1c952445ac625fb809df684396141818
Kerim Nezo <kerim.nezo@gmail.com>
2024-08-23 14:59:34 +0200

    Added base layout for property/show.blade.php so that I can start working on it
    As the summary says

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -3,9 +3,32 @@
         Property name
     </x-slot:title>
 
-    <main class="w-full h-full pt-32 bg-slate-500">
-        <div class="h-[500px]">
-            {{ $property->name }}
+    <main class="w-full h-full pt-32 bg-white text-black">
+        <div class="h-full w-full flex flex-col justify-center items-center">
+            <!-- Property photos -->
+            <div class="h-[500px] w-[80%]">
+                <div>
+                    {{ $property->name }}
+                </div>
+            </div>
+
+            <!-- Property/agent information -->
+            <div class="h-[200px] bg-green-400 w-[80%] rounded-[10px] flex justify-center items-center">
+                <!-- Property information -->
+                <div class="w-[50%] flex flex-col justify-center items-center">
+                    Cijena: {{ $property->price }} KM
+                </div>
+
+                <!-- Agents informations (podaci o "useru")-->
+                <div class="w-[50%] flex flex-col justify-center items-center">
+                    Agent: {{ $property->user->name }}
+                </div>
+            </div>
+
+            <!-- Similar properties -->
+            <div class="h-[200px] bg-orange-400 w-[80%] my-4 rounded-[10px]">
+                <!-- Ovdje ćemo dobiti samo listu propertya koji su bliski sa otvorenim propertyem koje ćemo izlistati -->
+            </div>
         </div>
     </main>
 </x-app-layout>
