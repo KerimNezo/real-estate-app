58296d30707f02fb9a23b2dd0ba126c6e1a9b34c
Kerim Nezo <kerim.nezo@gmail.com>
2024-08-24 23:16:42 +0200

    Added faker number to be set as price range 500-200k
    As the summary says

--- database/factories/PropertyFactory.php
+++ database/factories/PropertyFactory.php
@@ -17,7 +17,7 @@ class PropertyFactory extends Factory
     public function definition(): array
     {
         return [
-            'price' => 120000,
+            'price' => $this->faker->numberBetween(500, 200000),
             'surface' => 59,
             'lat' => 44.2,
             'lon' => 17.933333333333334,
