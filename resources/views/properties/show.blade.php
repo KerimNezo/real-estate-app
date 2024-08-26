dac7725efca062f40d345305d9d595fc7b47aa72
Kerim Nezo <kerim.nezo@gmail.com>
2024-08-26 15:17:36 +0200

    Edited show blade and app.css
    As the summary says

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -1,3 +1,8 @@
+@php
+    $mediaItems = $property->getMedia('property-photos');
+    $photoCount = count($mediaItems);
+@endphp
+
 <x-app-layout>
     <x-slot:title>
         Property name
@@ -6,17 +11,18 @@
     <main class="w-full h-full pt-32 bg-white text-black">
         <div class="h-full w-full flex flex-col justify-center items-center">
             <!-- Property photos -->
-            <div class="h-[500px] w-[80%]">
-                <div>
-                    {{ $property->name }}
-                </div>
+            <div class="w-[80%]">
+                <livewire:photo-gallery :mediaItems='$mediaItems' :photoCount='$photoCount'/>
             </div>
 
             <!-- Property/agent information -->
             <div class="h-[200px] bg-green-400 w-[80%] rounded-[10px] flex justify-center items-center">
                 <!-- Property information -->
                 <div class="w-[50%] flex flex-col justify-center items-center">
-                    Cijena: {{ $property->price }} KM
+                    {{ $property->name }} <br>
+                    Cijena: {{ $property->price }} KM <br>
+                    Ostali podaci <br>
+                    Mapa
                 </div>
 
                 <!-- Agents informations (podaci o "useru")-->
@@ -28,6 +34,9 @@
             <!-- Similar properties -->
             <div class="h-[200px] bg-orange-400 w-[80%] my-4 rounded-[10px]">
                 <!-- Ovdje ćemo dobiti samo listu propertya koji su bliski sa otvorenim propertyem koje ćemo izlistati -->
+                <div class="mt-4 ml-4">
+                    Similar properties
+                </div>
             </div>
         </div>
     </main>
