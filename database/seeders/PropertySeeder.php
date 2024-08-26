6093bd07592811832dc7ff9e4bd4506afe5ffbb0
Kerim Nezo <kerim.nezo@gmail.com>
2024-08-26 12:24:34 +0200

    Edited property seeder to add more pictures per property and lease.
    NOTE: need to add other tag values (furnished, video, floors, bedrooms,
    toilet...)

--- database/seeders/PropertySeeder.php
+++ database/seeders/PropertySeeder.php
@@ -14,6 +14,7 @@ class PropertySeeder extends Seeder
      */
     public function run(): void
     {
+        $path = '/home/rimke/code/diplomski/realestate-app/public/photos';
         // 1 - office, 2 - house, 3 - appartement
         // moram jos uraditi da se storea više slika po nekretnini i treba naći slike
         $propertyType = [1, 2, 3];
@@ -24,17 +25,29 @@ class PropertySeeder extends Seeder
         foreach ($propertyType as $type) {
             for ($x = 0; $x < 4; $x++) {
                 $street = ($x % 2 == 0) ? 'Mejdandžik 11' : 'Adolfa Goldberga 5';
+                $lease =  ($x % 2 == 0) ? 5 : null; // ili ce lease duraiton biti 5 ili null
 
                 $property = Property::factory()->create([
                     'name' => $propertyName[$type - 1],
+                    'lease_duration' => $lease,
                     'user_id' => User::factory()->create(),
                     'type_id' => Type::firstWhere('id', '=', $type),
                     'country' => 'Bosnia & Herzegovina',
                     'city' => $propertyCity[$x],
                     'street' => $street,
-                ])->addMedia('/home/rimke/code/diplomski/realestate-app/public/photos/'.$propertyPhoto[$type - 1].'.jpg')
+                ]);
+
+                $property->addMedia(''.$path.'/'.$propertyPhoto[$type-1].'s/'.$propertyPhoto[$type - 1].'0.jpg')
                     ->preservingOriginal()
                     ->toMediaCollection('property-photos');
+
+                // ovako dodajemo samo na nekretninu svaku još po 5 slika, ne onako kao prije
+                for ($y = 1; $y < 6; $y++) {
+                    $property->addMedia(''.$path.'/'.$propertyPhoto[$type-1].'s/'.$propertyPhoto[$type - 1].''.$y.'.jpg')
+                        ->preservingOriginal()
+                        ->toMediaCollection('property-photos');
+
+                }
             }
         }
     }
