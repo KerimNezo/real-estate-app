5d827c9fac46102246629fd175e69f28fb01bfb4
Kerim Nezo <kerim.nezo@gmail.com>
2024-08-30 15:13:05 +0200

    Added Leaflet map to property/show blade
    As the summary says

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -1,6 +1,9 @@
 @php
     $mediaItems = $property->getMedia('property-photos');
     $photoCount = count($mediaItems);
+    $lon = $property->lon;
+    $lat = $property->lat;
+    $propertyName = $property->name;
 @endphp
 
 <x-app-layout>
@@ -16,10 +19,10 @@
             </div>
 
             <!-- Property/agent information -->
-            <div class="h-full w-[80%] rounded-[10px] flex justify-center items-center py-5">
+            <div class="h-full w-[80%] rounded-[10px] flex justify-center items-center py-5 gap-4">
                 <!-- Property information -->
                 <div class="flex flex-col justify-center items-center w-[60%] gap-4">
-                    <div class="w-full h-full flex flex-col justify-center items-center bg-yellow-200 py-5 mr-4 rounded-[5px]">
+                    <div class="w-full h-full flex flex-col justify-center items-center bg-yellow-200 py-5 rounded-[5px]">
                         <div class="w-[50%] h-[15%] bg-white rounded-[5px] flex justify-start items-center" >
                             <h6 class="ml-4 text-3xl">{{ $property->name }}</h6>
                         </div>
@@ -29,14 +32,16 @@
                         Mapa
                     </div>
 
-                    <div class="w-full h-full flex flex-col justify-center items-center bg-yellow-200 py-5 mr-4 rounded-[5px]">
-                        <div id="map"></div>
+                    <div class="w-full h-full flex flex-col justify-center items-center rounded-[5px]">
+                        <div id="map" class="w-full rounded-[5px] border-[3px]">
+
+                        </div>
                     </div>
                 </div>
 
 
                 <!-- Agents informations (podaci o "useru")-->
-                <div class="w-[40%] h-full flex flex-col justify-center items-center bg-teal-300 py-5 rounded-[5px]">
+                <div class="w-[40%] h-full flex flex-col justify-center items-center bg-teal-300 mb-auto py-5 rounded-[5px]">
                     <div>
                         Agent: {{ $property->user->name }}
                     </div>
@@ -46,7 +51,7 @@
             <!-- Similar properties -->
             <div class="h-[200px] bg-orange-400 w-[80%] my-4 rounded-[10px]">
                 <!-- Ovdje ćemo dobiti samo listu propertya koji su bliski sa otvorenim propertyem koje ćemo izlistati -->
-                <div class="mt-4 ml-4">
+                <div class="mt-4 ml-4 w-full">
                     Similar properties
                 </div>
             </div>
@@ -57,8 +62,17 @@
 
 <script>
         // initialize the map on the "map" div with a given center and zoom
-    var map = L.map('map', {
-        center: [51.505, -0.09],
-        zoom: 13
-    });
+    var map = L.map('map');
+
+    map.setView([{{ $lat }}, {{ $lon }}], 17);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    var marker = L.marker([{{ $lat }}, {{ $lon }}]).addTo(map)
+        .bindPopup('{{ $propertyName }}')
+        .openPopup();
+
 </script>
