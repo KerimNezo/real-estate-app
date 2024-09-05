a9767e81a92984e7306bb5748b4ee1c8a05a0299
Kerim Nezo <kerim.nezo@gmail.com>
2024-09-05 14:54:37 +0200

    More details to show.blade page
    Added sticky div that hold agent data, above it div that containt price
    (both need more work)
    Map is kinda done, still parent div needs recoloring.
    Extra property details div (above map) isn't started it will be changed
    depending on the property type, will need to think about best way to do
    that.
    Price/agent div needs more work
    Main details div (property name, offerType, city, desc) needs polishing
    
    Haven't really started working on div that will display similar
    properties, and the algorithm still needs to be started >:D
    I think that is about it.
    
    Finish this asap bro... KN

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -5,11 +5,20 @@
     $lat = $property->lat;
     $propertyName = $property->name;
     $similarProperties = [$property, $property, $property];
+    if (is_null($property->lease_duration)) {
+        $propertyOffer = "FOR SELL";
+        $color = "red";
+    } else {
+        $propertyOffer = "FOR RENT";
+        $color = "sky";
+    }
+    $cijena = $property->price;
+    $price = number_format($cijena, 0, '.',',');
 @endphp
 
 <x-app-layout>
     <x-slot:title>
-        Property name
+        {{$propertyName}}
     </x-slot:title>
 
     <main class="w-full h-full pt-32 bg-white text-black">
@@ -20,20 +29,58 @@
             </div>
 
             <!-- Property/agent information -->
-            <div class="h-full w-[80%] rounded-[10px] flex justify-center items-center py-5 gap-4">
+            <div class="h-full w-[80%] rounded-[10px] flex justify-center items-center py-5 gap-4" >
                 <!-- Property information -->
-                <div class="flex flex-col justify-center items-center w-[60%] gap-4">
-                    <div class="w-full h-full flex flex-col justify-center items-center bg-yellow-200 py-5 rounded-[5px]">
-                        <div class="w-[50%] h-[15%] bg-white rounded-[5px] flex justify-start items-center" >
-                            <h6 class="ml-4 text-3xl">{{ $property->name }}</h6>
+                <div class="flex flex-col justify-center items-center w-[60%]">
+                    <!-- Property information -->
+                    <div class="flex flex-col justify-center items-center w-full gap-4 pb-2">
+                        <div class="px-5 py-5 w-full h-full flex flex-col justify-center items-center bg-yellow-200 rounded-[5px]">
+                            <!-- Property title and offer -->
+                            <div class="h-[15%] rounded-[5px] flex justify-start items-center pb-2 w-full">
+                                <h3 class="bg-{{$color}}-600 text-white rounded-[5px] p-2 font-bold flex justify-center items-center">
+                                    {{ $propertyOffer }}
+                                </h3>
+                                <h6 class="ml-4 text-3xl mr-auto">
+                                    {{ $property->name }}
+                                </h6>
+                            </div>
+
+                            <!-- Property location -->
+                            <div class="h-[15%] rounded-[5px] flex justify-start items-center pb-7 w-full">
+                                <h3 class="mr-auto">
+                                    {{ $property->city }}, {{ $property->country }}
+                                </h3>
+                            </div>
+
+                            <!-- Description title -->
+                            <div class="h-[15%] rounded-[5px] flex justify-start items-center pb-2 w-full">
+                                <h3 class="text-2xl mr-auto">
+                                    Description
+                                </h3>
+                            </div>
+
+                            <!-- Proprerty description content-->
+                            <div class="text-justify w-full">
+                                <p>
+                                    {{ $property->description }}
+                                </p>
+                            </div>
                         </div>
-                        <br>
-                        Cijena: {{ $property->price }} KM <br>
-                        Ostali podaci <br>
-                        Mapa
                     </div>
 
-                    <div class="w-full h-full flex flex-col justify-center items-center rounded-[5px]">
+                    <!-- Property details (sqrf, lease, price, other details..) -->
+                    <div class="flex flex-col justify-center items-center w-full gap-4 pb-2">
+                        <div class="px-5 py-10 w-full h-full flex flex-col justify-center items-center bg-yellow-200 rounded-[5px]">
+                            Dodatni detalji o nekretnini
+                        </div>
+                    </div>
+
+                    <!-- Property location on map -->
+                    <div class="w-full h-full flex flex-col justify-center items-center rounded-[5px] px-5 py-10 bg-yellow-200 ">
+                        <div class="text-2xl mr-auto pb-10">
+                            <b>Location</b>
+                        </div>
+
                         <div id="map" class="w-full rounded-[5px] border-[3px]">
 
                         </div>
@@ -42,15 +89,34 @@
 
 
                 <!-- Agents informations (podaci o "useru")-->
-                <div class="w-[40%] h-full flex flex-col justify-center items-center bg-teal-300 mb-auto py-5 rounded-[5px]">
-                    <div>
-                        <p>
-                            Agent: {{ $property->user->name }}
-                        </p>
+                <div class="w-[40%] min-h-screen relative mb-auto">
+                    <div class="w-full sticky top-[0] z-[100]">
+                        <div class="mb-[10px] h-[100px] bg-green-300 rounded-[5px] flex flex-col py-5 px-5 justify-center items-center">
+                            <!-- Property price -->
+                            <div class="rounded-[5px] flex justify-start items-center pb-2 w-full">
+                                <h6 class="text-xl mr-auto">
+                                    Price: ${{$price}}
+                                </h6>
+                            </div>
 
-                        <p>
-                            Contant us: {{ $property->user->phone_number }}
-                        </p>
+                            <!-- Lease duration -->
+                            <div class="h-[15%] rounded-[5px] flex justify-start items-center w-full">
+                                <h3 class="mr-auto">
+                                    {{ $property->city }}, {{ $property->country }}
+                                </h3>
+                            </div>
+                        </div>
+                        <div class="w-full flex flex-col justify-center items-center bg-teal-300 mb-auto py-5 rounded-[5px]">
+                            <div>
+                                <p>
+                                    Agent: {{ $property->user->name }}
+                                </p>
+
+                                <p>
+                                    Contant us: {{ $property->user->phone_number }}
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -80,6 +146,6 @@
     }).addTo(map);
 
     var marker = L.marker([{{ $lat }}, {{ $lon }}]).addTo(map)
-        .bindPopup('Vaša nekretnina')
+        .bindPopup('Your property')
         .openPopup();
 </script>
