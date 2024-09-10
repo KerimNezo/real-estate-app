b7be9c68cb84ff94783f43583ab2c72ab8f2c61f
Kerim Nezo <kerim.nezo@gmail.com>
2024-09-10 15:18:11 +0200

    Separated other features by property type
    Wanted to separate the feature section by property type if I ever wanted
    to add more features.

---
+++ resources/views/components/other-features-office.blade.php
@@ -0,0 +1,15 @@
+@props(['elevator', 'keycard'])
+
+@php
+    $keycard === null ? $keycard = "No" : $keycard = "Yes";
+    $elevator === null ? $elevator = "No" : $elevator = "Yes";
+@endphp
+
+<x-property-detail icon='phosphor-elevator' title="Elevator" :text="$elevator" unit="" css='w-[40px] h-[40px]' />
+
+<x-property-detail icon='phosphor-identification-card-light' title="Keycard entry" :text="$keycard" unit="" css='w-[40px] h-[40px]' />
+
+<!-- Empty div to push the second detail to the left, instead of this we can put other detail if we ever come up with one -->
+<div class="bg-[#ededed] w-full">
+
+</div>
