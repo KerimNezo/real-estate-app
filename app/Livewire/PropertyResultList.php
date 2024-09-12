5a18a79cb82592a769bee113194230bc33b22855
Kerim Nezo <kerim.nezo@gmail.com>
2024-09-12 16:03:34 +0200

    Edited propertyResultList controller
    Mostly worked on query trying to understand why does it execute extra
    time previous query. Don't understand why it is happening.
    NOTE: That still needs to be fixed. I really don't understand....

--- app/Livewire/PropertyResultList.php
+++ app/Livewire/PropertyResultList.php
@@ -2,23 +2,55 @@
 
 namespace App\Livewire;
 
+use App\Models\Property;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class PropertyResultList extends Component
 {
-    public $properties;
-    public $order;
-    public $propertyCount;
+    public $properties = [];
 
-    protected $listeners = ['filterUpdated' => 'updateProperties'];
+    public $propertyCount = 0;
 
+    public $query;
+
+    #[Computed]
+    #[On('form-submitted')]
     public function updateProperties($filters)
     {
-        // Use the $filters to query the properties
-        $this->properties = Property::query()
-            ->when($filters['location'], fn($query) => $query->where('city', $filters['location']))
-            // Add other filters...
-            ->get();
+        $this->reset(['query', 'properties', 'propertyCount']);
+
+        $query = Property::query()->with(['media' => function ($query) {
+            $query->limit(1);
+        }]);
+
+        if ($filters['location']) {
+            $query->where('city', '=', $filters['location']);
+        }
+
+        if ($filters['offer_type']) {
+            $query = match ($filters['offer_type']) {
+                '1' => $query->whereNull('lease_duration'), // sell
+                '2' => $query->whereNotNull('lease_duration'), // rent
+                default => $query,
+            };
+        }
+
+        if ($filters['property_type']) {
+            $query->where('type_id', '=', $filters['property_type']);
+        }
+
+        if ($filters['min_price']) {
+            $query->where('price', '>=', $filters['min_price']);
+        }
+
+        if ($filters['max_price']) {
+            $query->where('price', '<=', $filters['max_price']);
+        }
+
+        $this->properties = $query->get();
+        $this->propertyCount = $this->properties->count();
     }
 
     public function sortProperties($order)
@@ -29,63 +61,11 @@ class PropertyResultList extends Component
             default => 'created_at',
         };
 
-        $this->properties = $order === 'lowestfirst'
-            ? $this->properties->sortBy($sortBy)
-            : $this->properties->sortByDesc($sortBy);
+        $this->properties = $this->properties->sortBy($sortBy, SORT_REGULAR, $order === 'highestfirst');
     }
 
     public function render()
     {
         return view('livewire.property-result-list');
     }
-
-    /**
-     * public function fetchProperties()
-    {
-        $query = Property::query()
-        ->select('id', 'type_id', 'name', 'price', 'city', 'bedrooms', 'garage', 'furnished', 'floors', 'lease_duration', 'keycard_entry', 'surface', 'toilets')
-        ->with(['media' => function ($query) {
-            $query->limit(1);
-        }]);
-
-        if ($this->filters['location']) {
-            $query->where('city', '=', $this->filters['location']);
-        }
-
-        if ($this->filters['offer_type']) {
-            $query = match ($this->filters['offer_type']) {
-                '1' => $query->whereNull('lease_duration'), // sell
-                '2' => $query->whereNotNull('lease_duration'), // rent
-                default => $query,
-            };
-        }
-
-        if ($this->filters['property_type']) {
-            $query->where('type_id', '=', $this->filters['property_type']);
-        }
-
-        if ($this->filters['min_price']) {
-            $query->where('price', '>=', $this->filters['min_price']);
-        }
-
-        if ($this->filters['max_price']) {
-            $query->where('price', '<=', $this->filters['max_price']);
-        }
-
-        switch ($this->order) {
-            case 'lowestfirst':
-                $query->orderBy('price', 'asc');
-                break;
-            case 'highestfirst':
-                $query->orderBy('price', 'desc');
-                break;
-            default:
-                $query->latest();
-                break;
-        }
-
-        $this->properties = $query->get();
-        $this->propertyCount = $this->properties->count();
-    }
-     */
 }
