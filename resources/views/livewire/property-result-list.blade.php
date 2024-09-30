69d8c28ee655fb3cf4625fd18eb751dc1dd98050
Kerim Nezo <kerim.nezo@gmail.com>
2024-09-30 14:32:49 +0200

    Edited result list component
    as the summary says

--- resources/views/livewire/property-result-list.blade.php
+++ resources/views/livewire/property-result-list.blade.php
@@ -3,7 +3,7 @@
     <div id="properites-list" class="flex flex-col items-center justify-center w-full h-full">
         <!-- Header of query result section-->
         <div id="properites-list-header" class="flex flex-col items-center justify-center w-full h-full pt-3">
-            <p class="mr-auto text-lg text-black">We found {{ $properties->count() }} properties you</p>
+            <p class="mr-auto text-lg text-black">We found {{ count($properties) }} properties you</p>
 
             <div id="property-list-button" class="relative">
                 <select wire:model.defer="order" name="" id="dropdown-button" class="w-full !bg-[#ef5d60] h-[48px] rounded-[5px] px-2 text-base">
@@ -35,7 +35,7 @@
         <div class="w-full h-full py-5 text-black bg-white " id="featured-properties">
             <!-- Grid display of property cards -->
             <div class="text-xl text-center">
-                @if ($properties->isEmpty())
+                @if (empty($properties))
                     <div class="flex-col items-center justify-center mx-auto">
                         <div class="pt-[100px]">
                             <img src="./photos/icons/no-result.svg" alt="" class="w-[100px] mx-auto">
@@ -48,9 +48,9 @@
                 @else
                     <div class="grid w-full grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2"> <!-- ovdje sad treba editovati kako se pozicioniraju -->
                         @foreach ($properties as $property )
-                            <div wire:key="{{ $property->id }}">
-                                <a href="{{ route('single-property', ['id' => $property->id]) }}">
-                                    <x-property-card :$property/>
+                            <div wire:key="{{ $property['id'] }}">
+                                <a href="{{ route('single-property', ['id' => $property['id']]) }}">
+                                    <x-property-card :imageUrl='$property->getFirstMediaUrl("property-photos")' :$property/>
                                 </a>
                             </div>
                         @endforeach
