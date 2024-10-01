2372d526d4a66dd4665d19ee6a2c3c1876413aa3
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-01 09:46:29 +0200

    removed task comment
    as the summary says

--- resources/views/livewire/property-result-list.blade.php
+++ resources/views/livewire/property-result-list.blade.php
@@ -14,23 +14,6 @@
             </div>
         </div>
 
-        <!--When I reorder properties, it loads all of their photos, it makes 2 queries, one that queries the "snapped data"
-            other one that queries their photos from media table, i don't really know why does this happen or if it even should
-            happen in the first place. Will need to look this up tommorow. All you need to do is keep on working. You're back
-            form the vacation, time to work.
-
-
-            29.09. dodatak, read this again
-            https://livewire.laravel.com/docs/properties#eloquent-constraints-arent-preserved-between-requests
-            vidjet ćeš da ćeš morati eventualno preraditi logiku da pređeš na computed propertije. Jer kao što vidiš
-            nakon što livewire hydrira json snapshot u php na request, gubi se select constraint.
-
-            Ali opet ne znam kako će i hoće li se ovo moći uraditi, jer forma određuje constrint i neće uvijek biti isti
-            tako da se mora nekako skontati taj sistem da forma šalje sve isto, i vamo da se provjerava za null i constrainta
-            samo ono što nije null. Da bude computed, ali se onda mijenjaju sve rute koje si do sada radio.
-            Tako da ima posla.
-            Good luck, Kerim -->
-
         <!-- Content of query result section-->
         <div class="w-full h-full py-5 text-black bg-white " id="featured-properties">
             <!-- Grid display of property cards -->
