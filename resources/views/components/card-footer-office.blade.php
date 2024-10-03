9d9ab236db698dac1c5e0dcaac362dbae2fa92ec
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-03 12:13:44 +0200

    tailwind plackage auto re-ordered classes
    as the summary says

--- resources/views/components/card-footer-office.blade.php
+++ resources/views/components/card-footer-office.blade.php
@@ -1,29 +1,29 @@
 @props(['property'])
 
-<ul class="flex justify-start items-center gap-3 w-full px-6">
+<ul class="flex items-center justify-start w-full gap-3 px-6">
     @if (!is_null($property->surface))
-        <li class="flex justify-center items-center gap-1">
+        <li class="flex items-center justify-center gap-1">
             <x-phosphor-square-half-light class="w-[36px] h-[36px] fill-[#EF5D60]"/>
             <p class="text-sm">{{ $property->surface }} m&sup2; </p>
         </li>
     @endif
 
     @if (!is_null($property->garage))
-        <li class="flex justify-center items-center gap-1">
+        <li class="flex items-center justify-center gap-1">
             <x-bxs-car-garage class="w-[30px] h-[30px] fill-[#EF5D60]"/>
             <p class="text-sm">{{ $property->garage }}</p>
         </li>
     @endif
 
     @if (!is_null($property->floors))
-        <li class="flex justify-center items-center gap-1">
+        <li class="flex items-center justify-center gap-1">
             <x-bi-building class="w-[25px] h-[25px] text-[#EF5D60]"/>
             <p class="text-sm">{{ $property->floors }}</p>
         </li>
     @endif
 
     @if (!is_null($property->keycard_entry))
-        <li class="flex justify-center items-center gap-1">
+        <li class="flex items-center justify-center gap-1">
             <x-heroicon-s-credit-card class="w-[30px] h-[30px] text-[#EF5D60]"/>
             <p class="text-sm"></p>
         </li>
