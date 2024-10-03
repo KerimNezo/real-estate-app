52b3a0d1003d86096992ec0b901ca578aea0845e
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-03 10:50:26 +0200

    Automatic tailwind changes
    as the summary says

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-            <div class="relative min-h-screen flex flex-col items-center justify-center ">
+            <div class="relative flex flex-col items-center justify-center min-h-screen ">
                 <div class="relative w-full px-0">
                     @include('components.navbar-guest')
 
