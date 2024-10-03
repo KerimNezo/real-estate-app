5264e0c1de02b40ac504e395915d2ca803f37e7f
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-03 12:12:52 +0200

    Edited property detail css
    as the summary says

--- resources/views/components/property-detail.blade.php
+++ resources/views/components/property-detail.blade.php
@@ -1,11 +1,15 @@
 @props(['title', 'text', 'icon', 'unit', 'css'])
 
 <div id="property-detail" class="flex items-center justify-start w-[33%] ">
-    <div class="bg-white p-[2px] rounded-[10px] w-[50px] h-[50px] flex justify-center items-center" style="color: #EF5D60;">
+    <div class="bg-white p-[2px] rounded-[10px] !w-[50px] !h-[50px] flex justify-center items-center" style="color: #EF5D60;">
         @svg($icon, $css)
     </div>
     <div class="flex flex-col items-start justify-center">
-        <p class="pl-1 pr-1 text-base font-bold">{{ $title }}</p>
-        <p class="pl-1 pr-1 text-lg">{{ $text }} {!! $unit !!}</p>
+        <p id="details-title">{!! $title !!}</p>
+
+        <div class="flex">
+            <p id="details-text"> {!! $text !!} </p>
+            <p id="details-text"> {!! $unit !!}</p>
+        </div>
     </div>
 </div>
