579b35d462799a51cb62c3acceb3994ae708592c
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-03 14:56:13 +0200

    created layout component for admin

---
+++ resources/views/layouts/admin-layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="scroll-behavior: smooth;">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        @vite(['resources/js/app.js', 'resources/css/app.css'])
+
+        <title>{{ $title ?? 'Laravel'}}</title>
+
+        <!-- Fonts -->
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            <div class="relative flex flex-col items-center justify-center min-h-screen ">
+                <div class="relative w-full px-0">
+                    {{ $slot }}
+
+                    @include('components.property-page-footer')
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+<!-- Layout za obicnog korisnika page -->
