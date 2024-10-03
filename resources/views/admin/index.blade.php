0e8153a2b50fbd2d27d462fc833ee1ff2e350122
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-03 14:58:03 +0200

    Added index admin blade
    Home page of admin user

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,10 @@
+<x-admin-layout>
+    <x-slot:title>
+        Admin Panel
+    </x-slot:title>
+
+    <div class="h-[1200px] bg-slate-500 flex justify-center items-center">
+        Hello, Admin!
+    </div>
+</x-admin-layout>
+
