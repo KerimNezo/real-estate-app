e8202c643ad461d6a0e4b04bf91bb7624ff8d5e1
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-07 11:08:03 +0200

    Comented some footer icons
    There were properties with too many features that are mentioned in the
    footer, so on mobile they were taking up a lot of space. Might fix it up
    later on.

--- resources/views/components/card-footer-house.blade.php
+++ resources/views/components/card-footer-house.blade.php
@@ -1,39 +1,39 @@
 @props(['property'])
 
-<ul class="flex justify-start items-center gap-3 w-full px-6">
+<ul class="flex items-center justify-start w-full gap-3 px-6">
     @if (!is_null($property->bedrooms))
-        <li class="flex justify-center items-center gap-1">
+        <li class="flex items-center justify-center gap-1">
             <x-ionicon-bed class="w-[30px] h-[30px] fill-[#EF5D60]"/>
             <p class="text-sm">{{ $property->bedrooms }}</p>
         </li>
     @endif
 
     @if (!is_null($property->surface))
-        <li class="flex justify-center items-center gap-1">
+        <li class="flex items-center justify-center gap-1">
             <x-phosphor-square-half-light class="w-[36px] h-[36px] fill-[#EF5D60]"/>
             <p class="text-sm">{{ $property->surface }} m&sup2; </p>
         </li>
     @endif
 
     @if (!is_null($property->toilets))
-        <li class="flex justify-center items-center gap-1">
+        <li class="flex items-center justify-center gap-1">
             <x-eos-shower-o class="w-[32px] h-[32px] fill-[#EF5D60]"/>
             <p class="text-sm">{{ $property->toilets }}</p>
         </li>
     @endif
 
     @if (!is_null($property->garage))
-        <li class="flex justify-center items-center gap-1">
+        <li class="flex items-center justify-center gap-1">
             <x-bxs-car-garage class="w-[30px] h-[30px] fill-[#EF5D60]"/>
             <p class="text-sm">{{ $property->garage }}</p>
         </li>
     @endif
 
-    @if (!is_null($property->furnished))
-        <li class="flex justify-center items-center gap-1">
+    {{-- @if (!is_null($property->furnished))
+        <li class="flex items-center justify-center gap-1">
             <x-maki-furniture class="w-[26px] h-[26px] fill-[#EF5D60]"/>
             <p class="text-sm"></p>
         </li>
-    @endif
+    @endif --}}
 </ul>
 
