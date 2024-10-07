3d5659d5134a45eecd8fa3519140aa5ecc262d08
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-07 12:56:10 +0200

    Added newly created topbar component to show and index admin views
    as the summary says

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,41 +6,7 @@
     <!-- Page content -->
     <div class="flex flex-col items-center justify-center w-full h-full bg-gray-900">
         <!-- Admin topbar -->
-        <div class="flex items-center justify-center w-full py-6 bg-gray-800 h-[100px]">
-            <div class="relative flex items-center justify-center w-full px-10">
-                <div class="mr-auto">
-                    <a href="/">
-                        <img src={{ asset('photos/icons/logo1.svg')}} alt="alt" class="rounded-[5px]">
-                    </a>
-                </div>
-
-                <div class="font-bold">
-                    <p>{{ Auth::user()->name }}</p>
-                </div>
-
-                <button id="dugme" class="w-5 pl-12 pr-8 smy-auto" type="button">
-                    <x-heroicon-s-user class="h-8" />
-                </button>
-
-                <!-- dropdown which waits for a click on the user icon -->
-                <div id="desktop-admin-dropdown" class="mr-9 absolute right-0 top-[100%] mt-2 w-[200px] overflow-hidden bg-gray-700 rounded-lg shadow-lg transition-opacity duration-300"style="display: none; opacity: 0;">
-                    <!-- Logout button -->
-                    <div class="flex flex-col items-center justify-center w-full gap-5 px-6 py-4">
-                        <button type="button" onclick="event.preventDefault(); document.getElementById('goto-profile-form').submit();" class="flex items-center justify-center px-4 ml-auto">
-                            <p class="text-base">
-                                Profile
-                            </p>
-                        </button>
-
-                        <button type="button" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="flex items-center justify-center px-4 ml-auto">
-                            <p class="text-base">
-                                Logout
-                            </p>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('components.topbar-admin')
 
         <div class="h-[800px] w-full flex justify-center items-center z-9 pt-[100px]">
             <p class="mb-auto">welcome back to home page, {{ Auth::user()->name }}</p>
