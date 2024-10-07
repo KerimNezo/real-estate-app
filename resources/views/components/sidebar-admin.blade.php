5f9207b4d550331ac0e0933f11b86ae93d70b820
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-07 15:44:13 +0200

    Added sidebar component for admin view
    as the summary says

---
+++ resources/views/components/sidebar-admin.blade.php
@@ -0,0 +1,29 @@
+<!-- Sidebar -->
+<div class="fixed top-[100px] left-0 w-[220px] h-screen px-10 bg-gray-900 z-10 flex flex-col items-center justify-center">
+    <!-- Agents -->
+    <div class="mr-auto">
+        <x-section-title text="Agents" image="heroicon-s-user"/>
+
+        <x-section-option text="List" link="/agents/index"/>
+    </div>
+
+    <!-- Properties -->
+    <div class="mr-auto">
+        <x-section-title text="Properties" image="phosphor-house-line-fill"/>
+
+        <div class="flex flex-col items-center justify-center gap-4">
+            <x-section-option text="Houses" link="/property/houses"/>
+
+            <x-section-option text="Offices" link="/property/pffices"/>
+
+            <x-section-option text="Appartements" link="/property/appartements"/>
+        </div>
+    </div>
+
+    <!-- Dashboard -->
+    <div class="mb-auto mr-auto">
+        <x-section-title text="Dashboard" image="simpleline-graph"/>
+
+        <x-section-option text="Stats" link="/dashboard"/>
+    </div>
+</div>
