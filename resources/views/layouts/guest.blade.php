2e0c2dab2369a22b2b2d13d7b1fc5bea5e53827e
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-08 13:56:48 +0200

    reduced the width of the login div
    as the summary says

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,7 +22,7 @@
                 </a>
             </div>
 
-            <div class="w-[50%] sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-[35%] sm:max-w-md mt-10 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
