68039bcc15f4ae9cf6e86d645d668beb06bc8862
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-08 14:43:23 +0200

    Added template actions to AdminController for added routes
    as the summary says

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function showAgents()
+    {
+        $agents = User::query()
+            ->select('name', 'email', 'phone_number')
+            ->where('name', '!=', 'admin')
+            ->get();
+        //ovdje će se biti možda i profilna slika
+
+        return view('admin.agents')
+            ->with('agents', $agents);
+    }
+
+    public function showAgent($id)
+    {
+        $agent = User::query()
+            ->select('name', 'email', 'phone_number')
+            ->where('id', '==', $id)
+            ->get();
+        //ovdje će se biti možda i profilna slika
+
+        return view('admin.agent')
+            ->with('agent', $agent);
+    }
+
+    public function showHouses()
+    {
+        //query houses
+
+        return 'sve kuce';
+    }
+
+    public function showOffices()
+    {
+        //query offcies
+
+        return 'svi ofisi';
+    }
+
+    public function showAppartements()
+    {
+        //query appartements
+
+        return 'svi stanovi';
+    }
 }
