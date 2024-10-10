e87cabc2f120f3a7e6194c241d83ca4bade6fd95
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-10 12:24:43 +0200

    Updated table header text size
    as the summary says

--- resources/views/components/table/table-header.blade.php
+++ resources/views/components/table/table-header.blade.php
@@ -1,7 +1,9 @@
 @props(['title'])
 
-<th class="px-4 py-2 text-lg border-b border-gray-700">
+<th class="px-3 py-2 text-lg border-b border-gray-700">
     <div class="flex items-center justify-left">
-        {{ $title }}
+        <p class="text-lg">
+            {{ $title }}
+        </p>
     </div>
 </th>
