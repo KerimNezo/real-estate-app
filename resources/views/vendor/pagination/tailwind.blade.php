40ad40ca947a39d8adbf110a6a523bb116675048
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-10 12:28:20 +0200

    Edited pagination tailwind
    as the summary says

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,7 +1,7 @@
 @if ($paginator->hasPages())
     <div class="flex items-center justify-center w-full mt-2">
         {{-- Previous Page Link --}}
-        <div class="mr-auto text-sm">
+        <div class="text-sm">
             @if ($paginator->onFirstPage())
                 <span class="text-gray-500 disabled" aria-disabled="true">&laquo; Previous</span>
             @else
@@ -15,7 +15,7 @@
         </div>
 
         {{-- Next Page Link --}}
-        <div class="ml-auto text-sm">
+        <div class="text-sm">
             @if ($paginator->hasMorePages())
                 <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="text-[#ef5d60] hover:text-red-200">Next &raquo;</a>
             @else
