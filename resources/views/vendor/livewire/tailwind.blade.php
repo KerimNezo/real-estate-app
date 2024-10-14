8b6a6a317753157eea92c1f5231e362a2921288d
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-14 13:50:21 +0200

    Edited colors on pagination link
    as the summary says

--- resources/views/vendor/livewire/tailwind.blade.php
+++ resources/views/vendor/livewire/tailwind.blade.php
@@ -5,7 +5,7 @@
             @if ($paginator->onFirstPage())
                 <span class="text-gray-500 cursor-pointer disabled" aria-disabled="true" aria-label="{{ __('pagination.previous') }}">&laquo; Previous</span>
             @else
-                <a wire:click="previousPage('{{ $paginator->getPageName() }}')" aria-label="{{ __('pagination.previous') }}" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after" rel="prev" class="text-[#ef5d60] hover:text-red-200 cursor-pointer">&laquo; Previous</a>
+                <a wire:click="previousPage('{{ $paginator->getPageName() }}')" aria-label="{{ __('pagination.previous') }}" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after" rel="prev" class="text-[#ef5d60] hover:text-red-400 cursor-pointer">&laquo; Previous</a>
             @endif
         </div>
 
@@ -17,7 +17,7 @@
         {{-- Next Page Link --}}
         <div class="text-sm">
             @if ($paginator->hasMorePages())
-                <a wire:click="nextPage('{{ $paginator->getPageName() }}')" aria-label="{{ __('pagination.next') }}" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after" rel="next" class="text-[#ef5d60] hover:text-red-200 cursor-pointer">Next &raquo;</a>
+                <a wire:click="nextPage('{{ $paginator->getPageName() }}')" aria-label="{{ __('pagination.next') }}" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after" rel="next" class="text-[#ef5d60] hover:text-red-400 cursor-pointer">Next &raquo;</a>
             @else
                 <span class="text-gray-500 cursor-pointer disabled" aria-disabled="true" aria-label="{{ __('pagination.next') }}">Next &raquo;</span>
             @endif
