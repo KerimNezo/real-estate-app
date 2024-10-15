2dabaef56a5f540f26da84b794b2b67fca9f0934
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-15 12:44:20 +0200

    Changed margin for section titles on result table
    as the summary says

--- resources/views/components/section-title.blade.php
+++ resources/views/components/section-title.blade.php
@@ -1,6 +1,6 @@
 @props(['text', 'image'])
 
-<div class="flex items-center justify-center mt-10 mb-4">
+<div class="flex items-center justify-center mt-8 mb-4">
     @svg($image, ['class' => 'w-6 mr-2'])
     <p class="ml-[10px] mr-auto text-base">{{ $text }}</p>
 </div>
