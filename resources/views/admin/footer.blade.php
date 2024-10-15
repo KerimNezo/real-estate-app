5f7064c2084fe4abf87adcca63099523b25e6375
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-15 12:42:11 +0200

    Removed footer right padding that was there so it fixed its position to center, now that is not needed
    as the summary says

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -1,6 +1,6 @@
 <!--footer -->
 <div class="flex items-center justify-center mb-4">
     <p>
-        Powered by <a href="https://github.com/KerimNezo" target="_blank" class="pr-[200px] text-[#EF5D60] hover:text-red-400">Kerim Nezo</a>
+        Powered by <a href="https://github.com/KerimNezo" target="_blank" class="text-[#EF5D60] hover:text-red-400">Kerim Nezo</a>
     </p>
 </div>
