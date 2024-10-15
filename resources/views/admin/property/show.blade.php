67563d8a5a33357f6c7ee6eb7d0217ca2ddc7da0
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-15 14:36:24 +0200

    Added basic table data to the show blade, needed more editing
    as the summary says

--- resources/views/admin/property/show.blade.php
+++ resources/views/admin/property/show.blade.php
@@ -15,8 +15,49 @@
 
             <!-- Main content-->
             <div id="main-content" class="w-full pt-[100px] bg-gray-900">
-                <div class="h-[800px] w-full flex justify-center items-center z-9">
-                    <p class="mb-auto">welcome back to show property of id: {{ $property->id }} information page, {{ Auth::user()->name }}</p>
+                <div class="flex items-center justify-center w-full z-9">
+                    <!-- table to display property data -->
+                    <div class="py-8 text-xl text-center px-[6%]">
+                        <div class="overflow-x-auto">
+                            <table class="min-w-full overflow-hidden bg-gray-800 rounded-xl">
+                                <thead class="bg-gray-800 border-gray-700">
+                                    <tr id="table-header">
+                                        <!-- Image -->
+                                        <x-table.table-header title="Image" />
+                                        <!-- Agent -->
+                                        <x-table.table-header title="Agent" />
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($propertyAttributes as $key => $value)
+                                    <tr class="border-t border-gray-700">
+                                        <!-- Agent name -->
+                                        <td id="table-data">
+                                            <div class="flex items-center justify-start">
+                                                <p class="hover:text-white">
+                                                    {{ $key }}
+                                                </p>
+                                            </div>
+                                        </td>
+                                        <!-- Property price -->
+                                        <td id="table-data">
+                                            <div class="flex items-center justify-start">
+
+                                                <p>
+                                                    @if ($key === 'price')
+                                                    {{ number_format($value, 0) }} $
+                                                    @else
+                                                    {{ $value ?? 'no'}}
+                                                    @endif
+                                                </p>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
