75cb77c63cd64e3c833ae3b64acc2c04dc0d573b
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-15 16:41:22 +0200

    Added header of the table displaying Key and Data columns
    as the summary says

--- resources/views/admin/property/show.blade.php
+++ resources/views/admin/property/show.blade.php
@@ -14,40 +14,98 @@
             @include('components.sidebar-admin')
 
             <!-- Main content-->
-            <div id="main-content" class="w-full pt-[100px] bg-gray-900">
+            <div id="main-content" class="w-full bg-gray-900">
                 <div class="flex items-center justify-center w-full z-9">
                     <!-- table to display property data -->
                     <div class="py-8 text-xl text-center px-[6%]">
                         <div class="overflow-x-auto">
                             <table class="min-w-full overflow-hidden bg-gray-800 rounded-xl">
+                                <!-- Header of the table -->
                                 <thead class="bg-gray-800 border-gray-700">
                                     <tr id="table-header">
                                         <!-- Image -->
-                                        <x-table.table-header title="Image" />
+                                        <th class="py-2 pl-12 pr-3 text-lg border-b border-gray-700">
+                                            <div id="header-title">
+                                                <p class="text-lg">
+                                                    Key
+                                                </p>
+                                            </div>
+                                        </th>
+
                                         <!-- Agent -->
-                                        <x-table.table-header title="Agent" />
+                                        <th class="py-2 pl-12 pr-3 text-lg border-b border-gray-700">
+                                            <div id="header-title">
+                                                <p class="text-lg">
+                                                    Data
+                                                </p>
+                                            </div>
+                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($propertyAttributes as $key => $value)
+                                    <!-- media section -->
                                     <tr class="border-t border-gray-700">
-                                        <!-- Agent name -->
-                                        <td id="table-data">
+                                        <!-- Key -->
+                                        <td class="py-4 pl-12 text-base leading-6">
                                             <div class="flex items-center justify-start">
-                                                <p class="hover:text-white">
-                                                    {{ $key }}
+                                                <p class="text-left w-36">
+                                                    Media
                                                 </p>
                                             </div>
                                         </td>
-                                        <!-- Property price -->
-                                        <td id="table-data">
+                                        <!-- Value -->
+                                        <td class="py-4 pl-12 pr-4 text-base leading-6">
+                                            <div class="flex items-center justify-start space-x-2">
+                                                @foreach($property->getMedia('property-photos') as $media)
+                                                    <img src="{{ $media->getUrl() }}" alt="Property Photo" class="w-[120px] h-[90px] object-cover rounded-lg">
+                                                @endforeach
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <!-- user data -->
+                                    @foreach ($userData as $key => $value)
+                                        @if ($key === 'id')
+                                        <!-- Don't display his id -->
+                                        @else
+                                        <tr class="border-t border-gray-700">
+                                            <!-- Key -->
+                                            <td class="py-4 pl-12 text-base leading-6">
+                                                <div class="flex items-center justify-start">
+                                                    <p class="text-left w-36">
+                                                        {{ ucwords(str_replace('_', ' ', $key)) }}
+                                                    </p>
+                                                </div>
+                                            </td>
+                                            <!-- Value -->
+                                            <td class="py-4 pl-12 pr-4 text-base leading-6">
+                                                <div class="flex items-center justify-start">
+                                                    <p class="text-left">
+                                                        {{ $value }}
+                                                    </p>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        @endif
+                                    @endforeach
+                                    <!-- property data -->
+                                    @foreach ($propertyData as $key => $value)
+                                    <tr class="border-t border-gray-700">
+                                        <!-- Key -->
+                                        <td class="py-4 pl-12 text-base leading-6">
                                             <div class="flex items-center justify-start">
-
-                                                <p>
+                                                <p class="text-left w-36">
+                                                    {{ ucwords(str_replace('_', ' ', $key)) }}
+                                                </p>
+                                            </div>
+                                        </td>
+                                        <!-- Value -->
+                                        <td class="py-4 pl-12 pr-4 text-base leading-6">
+                                            <div class="flex items-center justify-start">
+                                                <p class="text-left">
                                                     @if ($key === 'price')
                                                     {{ number_format($value, 0) }} $
                                                     @else
-                                                    {{ $value ?? 'no'}}
+                                                    {{ $value ?? 'No'}}
                                                     @endif
                                                 </p>
                                             </div>
