7467223a812acd0bc2843f78b752881db5569061
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-17 15:07:43 +0200

    will start working on component controller
    as the summary says

--- app/Livewire/Admin/AgentIndex.php
+++ app/Livewire/Admin/AgentIndex.php
@@ -2,10 +2,15 @@
 
 namespace App\Livewire\Admin;
 
+use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\User;
 
 class AgentIndex extends Component
 {
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.admin.agent-index');
