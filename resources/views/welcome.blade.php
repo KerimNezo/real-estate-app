3787980cbbc8d72db813857811efd0f441cbf502
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-17 12:47:31 +0200

    minor sour eye change.
    ubadao me u oko ovaj space

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
             <!-- Grid display of property cards -->
             <div class="py-12 text-xl text-center">
                 <div class="px-[10%] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full"> <!-- ovdje sad treba editovati kako se pozicioniraju -->
-                    @foreach ($properties as $property )
+                    @foreach ($properties as $property)
                         <a href="{{ route('single-property', ['id' => $property->id]) }}">
                             <x-property-card :imageUrl='$property->getFirstMediaUrl("property-photos")' :$property/>
                         </a>
