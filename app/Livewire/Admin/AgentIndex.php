14128472d906891c17c895c07d061c2588e8cbf4
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-17 14:45:44 +0200

    Added agent-index livewire component but now at the right place
    as the summary says

---
+++ app/Livewire/Admin/AgentIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class AgentIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.agent-index');
+    }
+}
