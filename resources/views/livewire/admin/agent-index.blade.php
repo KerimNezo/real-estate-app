9f475ce98889b1b46596d660b9c0ce5217bb2072
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-17 22:38:21 +0200

    Removed anchor tag from image tag in user photo data in table
    as the summary says

--- resources/views/livewire/admin/agent-index.blade.php
+++ resources/views/livewire/admin/agent-index.blade.php
@@ -32,9 +32,7 @@
                             <td id="table-data">
                                 <div class="flex items-center justify-start">
                                     <p>
-                                        <a href=" {{ route('admin-single-agent', ['agent' => $property, 'user' => $property->user])}}">
-                                            <img src="{{ $property->getFirstMediaUrl('property-photos') }}" alt="Property Image" class="w-[100px] h-[75px] object-cover rounded-lg">
-                                        </a>
+                                        <img src="{{ $property->getFirstMediaUrl('property-photos') }}" alt="Property Image" class="w-[100px] h-[75px] object-cover rounded-lg">
                                     </p>
                                 </div>
                             </td> --}}
