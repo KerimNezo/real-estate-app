bd61e19be28264a9521a932b237b13cb249959b4
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-18 10:08:47 +0200

    Finished display of agent data on agent/show blade, later on will add list of his properties and his sales ?
    check comment below last <tr>

--- resources/views/admin/agent/show.blade.php
+++ resources/views/admin/agent/show.blade.php
@@ -1,6 +1,6 @@
 <x-admin-layout>
     <x-slot:title>
-        Admin Panel
+        Agent: {{ $agent->name }}
     </x-slot:title>
 
     <!-- Page content -->
@@ -14,10 +14,81 @@
             @include('components.sidebar-admin')
 
             <!-- Main content-->
-            <div id="main-content" class="w-full pt-[100px] bg-gray-900">
-                <div class="h-[600px] w-full flex justify-center items-center z-9">
-                    <p class="">welcome back to show agents {{ $agent->name }} information page, {{ Auth::user()->name }}</p>
-                    <p class="">phone number {{ $agent->phone_number }} </p>
+            <div id="main-content" class="w-full bg-gray-900">
+                <div class="flex flex-col items-center justify-center w-full z-9">
+                    <!-- table to display property agent data -->
+                    <div class="py-8 text-xl text-center px-[6%] w-full">
+                        <div class="w-full overflow-x-auto">
+                            <table class="min-w-full overflow-hidden bg-gray-800 rounded-xl">
+                                <!-- Header of the table -->
+                                <thead class="w-full bg-gray-800 border-gray-700">
+                                    <tr id="table-header" style="width: 100%">
+                                        <!-- Key -->
+                                        <th class="px-4 py-2 text-lg border-b border-gray-700">
+                                            <div class="flex items-center justify-start">
+                                                <p class="text-lg">Key</p>
+                                            </div>
+                                        </th>
+
+                                        <!-- Data -->
+                                        <th class="w-full px-4 py-2 text-lg border-b border-gray-700">
+                                            <div class="flex items-center justify-start">
+                                                <p class="text-lg">Data</p>
+                                            </div>
+                                        </th>
+                                    </tr>
+                                </thead>
+
+                                <!-- Table data -->
+                                <tbody class="w-full">
+                                    <!-- user data -->
+                                    <tr class="w-full border-t border-gray-700">
+                                        <!-- Id -->
+                                        <td class="px-4 py-4 text-base leading-6">
+                                            <div class="flex items-center justify-start">
+                                                <p class="text-left w-36">
+                                                    Avatar
+                                                </p>
+                                            </div>
+                                        </td>
+
+                                        <!-- Name -->
+                                        <td class="w-full px-4 py-4 text-base leading-6">
+                                            <div class="flex items-center justify-start">
+                                                <p class="text-left">
+                                                    <img src="{{ $agent->getFirstMediaUrl('agent-pfps') }}" alt="" class="rounded-[5px] h-[100px] w-[100px]">
+                                                </p>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                    @foreach ($agentData as $key=>$value)
+                                        <tr class="w-full border-t border-gray-700">
+                                            <!-- Id -->
+                                            <td class="px-4 py-4 text-base leading-6">
+                                                <div class="flex items-center justify-start">
+                                                    <p class="text-left w-36">
+                                                        {{ ucwords(str_replace('_', ' ', $key)) }}
+                                                    </p>
+                                                </div>
+                                            </td>
+
+                                            <!-- Name -->
+                                            <td class="w-full px-4 py-4 text-base leading-6">
+                                                <div class="flex items-center justify-start">
+                                                    <p class="text-left">
+                                                        {{ $value }}
+                                                    </p>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+
+                                    <!-- PROVJERI DA LI TI KAD PASSAS OVDJE USERA KUPI I NJEGOVE PROPERTYE COKI ARAZ RIJEŠTI TO SVE DANAS POŠAMARAJ -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
