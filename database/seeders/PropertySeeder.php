4d200da8168714eed83d4bf478ed46e44fd110a5
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-19 22:50:51 +0200

    Fixed seeder....

--- database/seeders/PropertySeeder.php
+++ database/seeders/PropertySeeder.php
@@ -61,7 +61,7 @@ class PropertySeeder extends Seeder
                     'video_intercom' => $elevator,
                     'street' => $street,
                     'description' => "If you have older kids or frequent guests, this home is meant for you. A spacious loft at the top of the stairs adjoins two bedrooms with walk-in closets and a shared bath, providing just the right amount of privacy for everyone.With an open-concept layout in the great room, kitchen and casual dining, there's plenty of space and seating to bring the whole family together. Stream your favorite videos. Adjust the lighting. Check the front door without leaving your comfy seat. Our HomeSmart features are included and connect tech to your device.",
-                    'status' => ''
+                    'status' => $status,
                 ]);
 
                 // Adding profile picture for a property agent
