ef4d8108018138df5f351351c3d9db536fb0880e
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-21 11:19:28 +0200

    Unset lon and lat for display on admin views
    i don't think it is neccesary for admin to have access to this data

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -71,7 +71,7 @@ class AdminController extends Controller
 
         $propertyData['Type'] = $this->changeTypeData($propertyData['type_id']);
 
-        unset($propertyData['type_id'], $propertyData['created_at'], $propertyData['updated_at'], $propertyData['user_id'], $propertyData['id']);
+        unset($propertyData['type_id'], $propertyData['created_at'], $propertyData['updated_at'], $propertyData['user_id'], $propertyData['id'], $propertyData['lon'], $propertyData['lat']);
 
         unset($userData['created_at'], $userData['updated_at'], $userData['email_verified_at'], $userData['password'], $userData['remember_token']);
 
