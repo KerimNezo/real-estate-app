8dd7047dad9e02ab5e71ccd8b9da9de231f2ce43
Kerim Nezo <kerim.nezo@gmail.com>
2024-10-25 17:21:48 +0200

    Useless I think but added width to full in photo gallery but it still should work. idk bout phone view...
    as the summary says

--- resources/views/livewire/photo-gallery.blade.php
+++ resources/views/livewire/photo-gallery.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="w-full h-full">
+    <div class="w-full">
         <!-- Primary photo -->
         <div class="w-full">
             <div class="cursor-pointer">
@@ -8,8 +8,8 @@
                 </div>
             </div>
 
-            <div wire:transition>
-                <img src="{{ $mediaItems[$id]->getUrl() }}" alt="slika" id="mainPhoto">
+            <div wire:transition class="w-full">
+                <img src="{{ $mediaItems[$id]->getUrl() }}" alt="slika" id="mainPhoto" class="w-full">
             </div>
 
             <div class="cursor-pointer">
@@ -22,7 +22,7 @@
         <!-- All photos -->
         <div class="w-full py-4">
             <div class="flex content-center justify-center gap-2">
-                @for ($i = 0; $i < 6; $i++)
+                @for ($i = 0; $i < $photoCount; $i++)
                     <img src="{{ $mediaItems[$i]->getUrl() }}" wire:click="makeMainPhoto({{$i}})" alt="slika" id="photoList">
                 @endfor
             </div>
