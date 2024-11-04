a36af0b193c6af7ae8b91a646e25a0eba59f5953
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-04 11:56:42 +0100

    Edited show blade display of property images.
    Made the list of photos x-scrollable and made their height the same.
    Made cursor pointer hidden on mobile

--- resources/views/livewire/photo-gallery.blade.php
+++ resources/views/livewire/photo-gallery.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full">
         <!-- Primary photo -->
         <div class="w-full">
-            <div class="cursor-pointer">
+            <div class="cursor-pointer" id="dugme-show-page">
                 <div id="dugmeLijevo" wire:click="previousPhoto()">
                     <
                 </div>
@@ -12,7 +12,7 @@
                 <img src="{{ $mediaItems[$id]->getUrl() }}" alt="slika" id="mainPhoto" class="w-full">
             </div>
 
-            <div class="cursor-pointer">
+            <div class="cursor-pointer" id="dugme-show-page">
                 <div id="dugmeDesno" class="ml-auto" wire:click="nextPhoto()">
                     >
                 </div>
@@ -20,10 +20,10 @@
         </div>
 
         <!-- All photos -->
-        <div class="w-full py-4">
-            <div class="flex content-center justify-center gap-2">
+        <div class="w-full max-w-full py-4 overflow-x-scroll">
+            <div class="flex items-center gap-2 whitespace-nowrap">
                 @for ($i = 0; $i < $photoCount; $i++)
-                    <img src="{{ $mediaItems[$i]->getUrl() }}" wire:click="makeMainPhoto({{$i}})" alt="slika" id="photoList">
+                    <img src="{{ $mediaItems[$i]->getUrl() }}" wire:click="makeMainPhoto({{ $i }})" alt="Photo" class="w-[230px] h-[140px] object-cover rounded-lg cursor-pointer">
                 @endfor
             </div>
         </div>
