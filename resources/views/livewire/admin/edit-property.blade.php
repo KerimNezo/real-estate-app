bbb4b2d071f93031cf81f3e302d745faf11d7c15
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-04 14:14:41 +0100

    Edited scroll on edit-property livewire component and will need to fix the width of the image div... same dumb issue as the other day....
    as the summary says

--- resources/views/livewire/admin/edit-property.blade.php
+++ resources/views/livewire/admin/edit-property.blade.php
@@ -4,28 +4,30 @@
         <!-- Title -->
         <h1 class="text-xl font-bold text-center">Update Property Information</h1>
 
-        <div class="w-full h-full px-20">
+        <div class="w-full h-full sm:px-20">
             <!-- Table to display property images -->
             <div class="w-full py-8 text-xl text-center">
                 <form wire:submit.prevent="uploadPhotos">
-                    <div class="flex items-center justify-center gap-4 pb-4">
+                    <div class="flex flex-col items-center justify-center gap-4 pb-4 sm:flex-row">
                         <p class="mr-auto text-lg font-bold text-left">
                             Property images:
                         </p>
 
-                        <button type="button" onclick="document.getElementById('file-upload').click()" class="px-3 py-2 ml-auto text-base text-white bg-green-600 rounded-lg">
-                            New photo
-                        </button>
+                        <div class="flex items-center justify-center gap-4 mr-auto sm:mr-0">
+                            <button type="button" onclick="document.getElementById('file-upload').click()" class="px-3 py-2 ml-auto text-base text-white bg-green-600 rounded-lg">
+                                <p>New photo</p>
+                            </button>
 
-                        <input type="file" multiple id="file-upload" class="hidden" wire:model="newPhotos">
+                            <input type="file" multiple id="file-upload" class="hidden" wire:model="newPhotos">
 
-                        <button type="button" wire:click="resetPhotos" class="flex items-center justify-center gap-2 px-3 py-2 text-base text-white bg-blue-600 rounded-lg">
-                            <div wire:loading>
-                                <img src="{{ asset('photos/spinner.svg') }}" wire:loading class="w-6 h-6"></img> <!-- SVG loading spinner -->
-                            </div>
+                            <button type="button" wire:click="resetPhotos" class="flex items-center justify-center gap-2 px-3 py-2 text-base text-white bg-blue-600 rounded-lg">
+                                <div wire:loading wire:target="resetPhotos">
+                                    <img src="{{ asset('photos/spinner.svg') }}" wire:loading class="w-6 h-6"></img> <!-- SVG loading spinner -->
+                                </div>
 
-                            <p>Reset photos</p>
-                        </button>
+                                <p>Reset photos</p>
+                            </button>
+                        </div>
                     </div>
                     <div class="max-w-full overflow-x-scroll">
                         <div class="flex items-center justify-start w-full max-w-full space-x-2 overflow-x-scroll whitespace-nowrap">
@@ -85,7 +87,7 @@
             <!-- Table to display other property data-->
             <div>
                 <!-- Property data -->
-                <div class="flex w-full h-full gap-4">
+                <div class="flex flex-col w-full h-full gap-4 sm:flex-row">
                     <!-- General property data -->
                     <div class="w-full">
                         <!-- Property agent -->
