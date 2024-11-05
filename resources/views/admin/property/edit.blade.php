d3f83d321d0daefa7dc7cddfbfddd69a9a16582b
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-05 13:44:43 +0100

    Re added max-w-[100vw] to edit property blade, so the width doesn't change based on the number of photos
    as the summary says

--- resources/views/admin/property/edit.blade.php
+++ resources/views/admin/property/edit.blade.php
@@ -14,7 +14,7 @@
             @include('components.sidebar-admin')
 
             <!-- Main content -->
-            <div id="main-content" class="flex flex-col flex-grow w-full pt-6 bg-gray-900">
+            <div id="main-content" class="flex flex-col flex-grow w-full pt-6 bg-gray-900 !max-w-[100vw]">
                 <div class="w-[80%] mx-auto py-8">
                     <!-- Livewire form -->
                     <livewire:admin.edit-property :$property :$propertyMedia/>
