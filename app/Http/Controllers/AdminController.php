7898322c28a5f1966f8b4e77872b87f0c9656416
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-06 15:13:16 +0100

    Removed 'id' from user data being passed on to show-agent.blade.php
    as the summary says. Just don't think admin needs or is relevant for
    admin to have insight in agents id from db.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -17,7 +17,7 @@ class AdminController extends Controller
     {
         $agentData = $user->getAttributes();
 
-        unset($agentData['created_at'], $agentData['updated_at'], $agentData['email_verified_at'], $agentData['password'], $agentData['remember_token']);
+        unset($agentData['id'], $agentData['created_at'], $agentData['updated_at'], $agentData['email_verified_at'], $agentData['password'], $agentData['remember_token']);
 
         return view('admin.agent.show')
             ->with('agent', $user)
