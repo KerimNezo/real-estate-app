0b901d5aca669b459168c808f08c7e403a9d4778
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-12 16:09:58 +0100

    Added validation, seems to work, but trying to make validation error message dissapear after some time. doesn't seem to work rn. Will remove it probably.

--- app/Livewire/Admin/AgentPicture.php
+++ app/Livewire/Admin/AgentPicture.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin;
 
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 use Livewire\WithFileUploads;
 
 class AgentPicture extends Component
@@ -12,18 +13,41 @@ class AgentPicture extends Component
 
     public $agentPicture;
 
-    public $newPhoto = '';
+    #[Validate]
+    public $newPhoto;
 
     public $tempPhoto;
+
+    public $tempNewPhoto;
 
     public function mount()
     {
         $this->tempPhoto = $this->agentPicture;
     }
 
-    public function resetPhoto()
+    public function isImage($file)
     {
-        $this->newPhoto = '';
+        return $file && in_array($file->getMimeType(), ['image/jpeg', 'image/png', 'image/webp', 'image/jpeg']);
     }
 
+    public function rules()
+    {
+        return [
+            'newPhoto' => 'required|mimetypes:image/jpg,image/jpeg,image/png,image/webp|max:1024',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'newPhoto.required' => 'Please upload an image file.',
+            'newPhoto.mimetypes' => 'Only JPG, JPEG, PNG, and WEBP files are allowed.',
+            'newPhoto.max' => 'The image size must be less than 1MB.',
+        ];
+    }
+
+    public function resetPhoto()
+    {
+        $this->newPhoto = null;
+    }
 }
