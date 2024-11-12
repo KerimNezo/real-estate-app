a89cb31ef6697c6ebe012205886ac206e3cd8a41
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-12 12:01:33 +0100

    removed AlpineJS code and edited the form a bit.
    as the summary says

--- resources/views/livewire/admin/agent-picture.blade.php
+++ resources/views/livewire/admin/agent-picture.blade.php
@@ -1,12 +1,13 @@
+
 <div>
     <div class="flex flex-col items-center justify-center w-full">
         <!-- Photo gallery header with buttons -->
-        <div class="flex flex-col items-center justify-center w-full gap-4 pb-4 sm:flex-row">
-            <p class="w-full mr-auto text-lg font-bold text-left">
+        <div class="flex flex-col items-center justify-center w-full pb-4 sm:flex-row">
+            <p class="mr-auto text-lg font-bold text-left whitespace-nowrap">
                 Profile picture:
             </p>
 
-            <div class="flex items-center justify-center w-full gap-4 mr-0">
+            <div class="flex items-center justify-center w-full gap-4 pt-4 mr-0 sm:pt-0">
                 <button type="button" onclick="document.getElementById('file-upload').click()" class="px-3 py-2 ml-0 mr-auto text-base text-white bg-green-600 rounded-lg sm:ml-auto sm:mr-0">
                     <p>New photo</p>
                 </button>
@@ -23,31 +24,26 @@
             </div>
         </div>
 
-        <div class="flex items-center justify-center w-full pt-10">
-            <div class="relative" x-data="{ isLoading: true }" x-init="
-                $watch('isLoading', (value) => {
-                    if (!value && newPhoto) {
-                        isLoading = true;
-                        const img = new Image();
-                        img.src = '{{ $newPhoto ? $newPhoto->temporaryUrl() : '' }}';
-                        img.onload = () => { isLoading = false; }
-                    }
-                });
-            ">
+        <div class="flex items-center justify-center w-full py-10">
+            <div class="relative">
                 @if ($newPhoto === '')
-                    <img src="{{ $tempPhoto->getUrl() }}" alt="Agent's profile picture" class="bg-gray-800 border-4 border-gray-900 h-60">
+                    <img src="{{ $tempPhoto->getUrl() }}" alt="Agent's profile picture" class="bg-gray-800 border-4 border-gray-900 sm:h-72 h-52">
                 @else
-                    <img src="{{ $newPhoto->temporaryUrl() }}" alt="Agent's profile picture"
-                        x-on:load="isLoading = false"
-                        class="bg-gray-800 border-4 border-gray-900 h-60">
+                    <img src="{{ $newPhoto->temporaryUrl() }}" alt="Agent's profile picture" class="bg-gray-800 border-4 border-gray-900 sm:h-72 h-52">
                 @endif
 
                 <!-- Loader Overlay, shown while newPhoto is being updated or loading -->
-                <div x-show="isLoading" wire:loading wire:target="newPhoto"
-                    class="absolute inset-0 z-10 flex items-center justify-center bg-gray-700 bg-opacity-75">
-                    <img src="{{ asset('photos/spinner.svg') }}" alt="Loading" class="h-60 opacity-80">
+                <div wire:loading.delay wire:target="newPhoto" class="absolute inset-0 z-10 flex items-center justify-center bg-gray-700 bg-opacity-75 px-auto">
+                    <img src="{{ asset('photos/spinner.svg') }}" alt="Loading" class="w-full sm:h-72 h-52 opacity-80 pl-auto pr-auto">
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        function showSpinner() {
+            document.getElementById('spinner').classList.remove('hidden')';
+            document.getElementById('spinner').classList.add('flex')';
+        }
+    </script>
 </div>
