3c24c326c291adb8131a1a603f8e8c0fe7d25018
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-12 12:00:20 +0100

    Shifted padding top on main content div and button to py and removed comments
    as the summary says

--- resources/views/admin/agent/edit.blade.php
+++ resources/views/admin/agent/edit.blade.php
@@ -14,7 +14,7 @@
             @include('components.sidebar-admin')
 
             <!-- Main content -->
-            <div id="main-content" class="w-full pt-6 bg-gray-900">
+            <div id="main-content" class="w-full py-6 bg-gray-900">
                 <!-- Update Property Form -->
                 <div>
                     <!-- Update Property Form -->
@@ -23,88 +23,6 @@
                         <h1 class="py-4 text-xl font-bold text-center">Update Agent Information</h1>
 
                         <div class="w-full h-full px-4">
-                            <!-- Table to display property images -->
-                            {{-- <div class="w-full py-8 text-xl text-center">
-                                <form wire:submit.prevent="uploadPhotos">
-                                    <!-- Photo gallery header with buttons -->
-                                    <div class="flex flex-col items-center justify-center gap-4 pb-4 sm:flex-row">
-                                        <p class="w-full mr-auto text-lg font-bold text-left whitespace-normal">
-                                            Property images:
-                                        </p>
-
-                                        <div class="flex items-center justify-center w-full gap-4 mr-0">
-                                            <button type="button" onclick="document.getElementById('file-upload').click()" class="px-3 py-2 ml-0 mr-auto text-base text-white bg-green-600 rounded-lg sm:ml-auto sm:mr-0">
-                                                <p>New photo</p>
-                                            </button>
-
-                                            <input type="file" multiple id="file-upload" class="hidden" wire:model="newPhotos">
-
-                                            <button type="button" wire:click="resetPhotos" class="flex items-center justify-center gap-2 px-3 py-2 text-base text-white bg-blue-600 rounded-lg">
-                                                <div wire:loading wire:target="resetPhotos">
-                                                    <img src="{{ asset('photos/spinner.svg') }}" wire:loading class="w-6 h-6"></img> <!-- SVG loading spinner -->
-                                                </div>
-
-                                                <p>Reset photos</p>
-                                            </button>
-                                        </div>
-                                    </div>
-
-                                    <!-- Property photos -->
-                                    <div class="max-w-full overflow-x-scroll">
-                                        <div class="flex items-center justify-start w-full max-w-full space-x-2 overflow-x-scroll whitespace-nowrap">
-                                            <!-- Display photos from database -->
-                                            @foreach($tempPhotos as $media)
-                                                <div class="relative" wire:key="{{ $media->id }}">
-                                                    <img src="{{ $media->getUrl() }}"
-                                                        alt="Property Photo"
-                                                        class="w-[150px] h-[90px] object-cover rounded-lg cursor-pointer">
-
-                                                    <button type="button" class="absolute flex items-center justify-center w-5 h-5 text-xs text-black bg-white rounded-full top-1 right-1"
-                                                            wire:click="removePhoto({{ $media->order_column }}, {{ $media->id }})"  wire:loading.attr="disabled">
-                                                        <div class="flex items-center justify-center pb-[2px] font-bold">
-                                                            <p>x</p>
-                                                        </div>
-                                                    </button>
-
-                                                    <div wire:loading wire:target="removePhoto({{ $media->order_column }}, {{ $media->id }})"
-                                                        class="absolute inset-0 z-10 flex items-center justify-center bg-gray-700 bg-opacity-75 rounded-lg">
-                                                        <img src="{{ asset('photos/spinner.svg') }}" alt="Loading" class="w-[150px] h-[90px] opacity-75">
-                                                    </div>
-                                                </div>
-                                            @endforeach
-
-                                            <!-- Display new photo previews -->
-                                            @foreach($newPhotoArray as $key => $photo)
-                                                <div class="relative" wire:key="{{ $key }}">
-                                                    <img src="{{ $photo->temporaryUrl() }}"
-                                                        alt="Loading Photo"
-                                                        class="w-[150px] h-[90px] object-cover rounded-lg cursor-pointer">
-
-                                                    <button type="button" class="absolute flex items-center justify-center w-5 h-5 text-xs text-black bg-white rounded-full top-1 right-1"
-                                                            wire:click="removeNewPhoto('{{ $key }}')" wire:loading.attr="disabled">
-                                                        <div class="flex items-center justify-center pb-[2px] font-bold">
-                                                            <p>x</p>
-                                                        </div>
-                                                    </button>
-
-                                                    <div wire:loading wire:target="removeNewPhoto('{{ $key }}')"
-                                                        class="absolute inset-0 z-10 flex items-center justify-center bg-gray-700 bg-opacity-75 rounded-lg">
-                                                        <img src="{{ asset('photos/spinner.svg') }}" alt="Loading" class="w-[150px] h-[90px] opacity-75">
-                                                    </div>
-                                                </div>
-                                            @endforeach
-                                        </div>
-                                    </div>
-                                    @error('newPhotos')
-                                        <p class="text-sm text-red-500">{{ $message }}</p>
-                                    @enderror
-
-                                    @error('newPhotoPreviews')
-                                        <div class="text-red-500">{{ $message }}</div>
-                                    @enderror
-                                </form>
-                            </div> --}}
-
                             <!-- Table to display agent data-->
                             <div class="flex flex-col w-full gap-4 lg:flex-row">
                                 <!-- General property data -->
@@ -134,7 +52,7 @@
                             <!-- Update Button -->
                             <form class="" enctype="multipart/form-data">
                                 @csrf
-                                <div class="pt-6 text-center">
+                                <div class="py-6 text-center">
                                     <button type="submit"  class="px-6 py-2 text-white bg-blue-600 rounded-lg">
                                         Update Agent
                                     </button>
