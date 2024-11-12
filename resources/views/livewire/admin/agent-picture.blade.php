f87c37e8741bb29408bfa805afe6031b7be03045
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-12 16:11:32 +0100

    Removed some things and reorganizied the way photo is displayed. Will need to think it through, but it's functional for now.
    as the summary says. Focus on shipping (finishing) the project. Do the
    details later, when it is stable. No1's gonna use it anyway. Livewire's
    a pain.

--- resources/views/livewire/admin/agent-picture.blade.php
+++ resources/views/livewire/admin/agent-picture.blade.php
@@ -12,7 +12,7 @@
                     <p>New photo</p>
                 </button>
 
-                <input type="file" id="file-upload" class="hidden" wire:model="newPhoto">
+                <input type="file" id="file-upload" class="hidden" wire:model="newPhoto" />
 
                 <button type="button" wire:click="resetPhoto" class="flex items-center justify-center gap-2 px-3 py-2 text-base text-white bg-blue-600 rounded-lg">
                     <div wire:loading wire:target="resetPhoto">
@@ -24,26 +24,23 @@
             </div>
         </div>
 
-        <div class="flex items-center justify-center w-full py-10">
+        <div class="flex flex-col items-center justify-center w-full py-8">
             <div class="relative">
-                @if ($newPhoto === '')
-                    <img src="{{ $tempPhoto->getUrl() }}" alt="Agent's profile picture" class="bg-gray-800 border-4 border-gray-900 sm:h-72 h-52">
+                @if ($this->isImage($newPhoto))
+                    <img src="{{ $newPhoto->temporaryUrl() }}" alt="Agent's new profile picture" class="bg-gray-800 border-4 border-gray-900 sm:h-72 h-52">
                 @else
-                    <img src="{{ $newPhoto->temporaryUrl() }}" alt="Agent's profile picture" class="bg-gray-800 border-4 border-gray-900 sm:h-72 h-52">
+                    <img src="{{ $tempPhoto->getUrl() }}" alt="Agent's profile picture" class="bg-gray-800 border-4 border-gray-900 sm:h-72 h-52">
                 @endif
 
                 <!-- Loader Overlay, shown while newPhoto is being updated or loading -->
-                <div wire:loading.delay wire:target="newPhoto" class="absolute inset-0 z-10 flex items-center justify-center bg-gray-700 bg-opacity-75 px-auto">
+                <div wire:loading wire:target="newPhoto" class="absolute inset-0 z-10 flex items-center justify-center bg-gray-700 bg-opacity-75 px-auto">
                     <img src="{{ asset('photos/spinner.svg') }}" alt="Loading" class="w-full sm:h-72 h-52 opacity-80 pl-auto pr-auto">
                 </div>
             </div>
+
+            @error('newPhoto')
+                <p id="error-message" class="pt-4 text-sm text-red-500">{{ $message }}</p>
+            @enderror
         </div>
     </div>
-
-    <script>
-        function showSpinner() {
-            document.getElementById('spinner').classList.remove('hidden')';
-            document.getElementById('spinner').classList.add('flex')';
-        }
-    </script>
 </div>
