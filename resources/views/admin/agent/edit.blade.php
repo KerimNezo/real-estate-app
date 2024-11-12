9896b40e0efc13c78879b1f12ae93e8f30fbc144
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-12 22:22:39 +0100

    Added some js error handling will ask tommorow should I continue using this or just stick with the livewire

--- resources/views/admin/agent/edit.blade.php
+++ resources/views/admin/agent/edit.blade.php
@@ -31,18 +31,22 @@
                                 <div class="w-full sm:w-[40%] pt-[10px]">
                                     <!-- Agent Name -->
                                     <x-formInput type="text" title="name" label="Name" :value='$agent->name'/>
+                                    <p id="nameError" class="hidden pb-2 text-red-600 text-bold">You didn't enter any name.</p>
 
                                     <!-- Agent email -->
                                     <x-formInput type="text" title="agentEmail" label="Email" :value='$agent->email'/>
+                                    <p id="emailError" class="hidden pb-2 text-red-600 text-bold">Your email isn't valid.</p>
 
                                     <!-- Agent phone number -->
                                     <x-formInput type="text" title="phoneNumber" label="Phone number" :value='$agent->phone_number'/>
+                                    <p id="phoneError" class="hidden pb-2 text-red-600 text-bold">You didn't enter a phone number</p>
 
                                     <!-- Agent password -->
                                     <x-formInput type="password" title="agentPassword" label="New password" />
 
                                     <!-- Confirm agent password -->
                                     <x-formInput type="password" title="confirmAgentPassword" label="Confirm new password" />
+                                    <p id="passwordError" class="hidden pb-2 text-red-600 text-bold">Your passwords don't match.</p>
                                 </div>
 
                                 <!-- Agent profile picture section (will be a livewire component) -->
@@ -52,20 +56,58 @@
                             </div>
 
                             <!-- Update Button -->
-                            <form class="" enctype="multipart/form-data" action="{{ route('update-agent', ['user' => $agent]) }}" method="POST" >
+                            <form id="updateForm" class="hidden" enctype="multipart/form-data" action="{{ route('update-agent', ['user' => $agent]) }}" method="POST">
+                                @method('PUT')
                                 @csrf
-
-                                <div class="py-6 text-center">
-                                    <button type="submit"  class="px-6 py-2 text-white bg-blue-600 rounded-lg">
-                                        Update Agent
-                                    </button>
-                                </div>
                             </form>
+
+                            <div class="py-6 text-center">
+                                <button onclick="submitForm()" class="px-6 py-2 text-white bg-blue-600 rounded-lg">
+                                    Update Agent
+                                </button>
+                            </div>
+
                         </div>
                     </div>
                 </div>
+                {{-- <livewire:admin.edit-agent /> --}}
             </div>
         </div>
+
+        <script>
+            function submitForm() {
+
+                console.log('verify input');
+                verify();
+
+                //document.getElementById('updateForm').submit();
+            }
+
+            function verify() {
+                // function that verifies input
+                const name = document.getElementById('name').value;
+                const agentEmail = document.getElementById('agentEmail').value;
+                const phoneNumber = document.getElementById('phoneNumber').value;
+                const agentPassword = document.getElementById('agentPassword').value;
+                const confirmAgentPassword = document.getElementById('confirmAgentPassword').value;
+
+                if (agentPassword !== confirmAgentPassword) {
+                    document.getElementById('passwordError').classList.remove('hidden');
+                    setTimeout(() => {
+                        document.getElementById('passwordError').classList.add('hidden');
+                    }, 5000);
+                }
+
+                if (name === '') {
+                    document.getElementById('nameError').classList.remove('hidden');
+                    setTimeout(() => {
+                        document.getElementById('nameError').classList.add('hidden');
+                    }, 5000);
+                }
+
+                console.log('proslo je');
+            }
+        </script>
 
         @include('admin.footer')
     </div>
