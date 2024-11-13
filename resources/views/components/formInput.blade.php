1b7c4db0b32168f1cbefafc4bb56c718a9373182
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-13 15:31:20 +0100

    Edited formInput to assign wire:model to inputs and make it work with livewire component
    as the summary says

--- resources/views/components/formInput.blade.php
+++ resources/views/components/formInput.blade.php
@@ -1,9 +1,8 @@
 @props(['title', 'label', 'model' => '', 'type'])
 
-<div class="mb-4 mr-auto">
+<div class="mb-2 mr-auto">
     <label for="{{ $title }}" class="block mb-2 font-bold">{{ $label }}:</label>
-    <input type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg" wire:model="{{ $model }}">
-    @error('{{ $title }}')
-        <p class="text-sm text-red-500">{{ $message }}</p>
-    @enderror
+    <input type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg @error($title)
+        border-red-500
+    @enderror" wire:model.blur="{{ $model }}">
 </div>
