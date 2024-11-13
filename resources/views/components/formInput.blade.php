aee86fb86cfd83768feb0f41a581c37dc6d67d60
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-13 13:07:06 +0100

    Added wire:model to formInput because it will be used in newly created livewire component (agent-form) that I will push later

--- resources/views/components/formInput.blade.php
+++ resources/views/components/formInput.blade.php
@@ -1,8 +1,8 @@
-@props(['title', 'label', 'value' => '', 'type'])
+@props(['title', 'label', 'model' => '', 'type'])
 
 <div class="mb-4 mr-auto">
     <label for="{{ $title }}" class="block mb-2 font-bold">{{ $label }}:</label>
-    <input type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg" value="{{ $value }}">
+    <input type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg" wire:model="{{ $model }}">
     @error('{{ $title }}')
         <p class="text-sm text-red-500">{{ $message }}</p>
     @enderror
