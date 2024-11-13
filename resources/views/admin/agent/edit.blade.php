a26e28b257e29c9819458450ba7cf5c27ed4e6b0
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-13 16:00:20 +0100

    Added form above both livewire components
    as the summary says. Will need to make this work, ofc, probably won't
    accept data like this, but we'll see.

--- resources/views/admin/agent/edit.blade.php
+++ resources/views/admin/agent/edit.blade.php
@@ -25,25 +25,24 @@
                         </h1>
 
                         <div class="w-full h-full px-4">
-                            <!-- Table to display agent data-->
-                            <div class="flex flex-col w-full gap-4 lg:flex-row">
-                                <!-- General property data -->
-                                <div class="w-full sm:w-[40%] pt-[10px]">
-                                    <livewire:admin.agent-form :$agent />
-                                </div>
-
-                                <!-- Agent profile picture section (will be a livewire component) -->
-                                <div class="w-full sm:w-[60%]">
-                                    <livewire:admin.agent-picture :$agentPicture />
-                                </div>
-                            </div>
-
-                            <!-- Update Button -->
                             <form id="updateForm" class="hidden" enctype="multipart/form-data" action="{{ route('update-agent', ['user' => $agent]) }}" method="POST">
                                 @method('PUT')
                                 @csrf
+                                <!-- Table to display agent data-->
+                                <div class="flex flex-col w-full gap-4 lg:flex-row">
+                                    <!-- General property data -->
+                                    <div class="w-full sm:w-[40%] pt-[10px]">
+                                        <livewire:admin.agent-form :$agent />
+                                    </div>
+
+                                    <!-- Agent profile picture section (will be a livewire component) -->
+                                    <div class="w-full sm:w-[60%]">
+                                        <livewire:admin.agent-picture :$agentPicture />
+                                    </div>
+                                </div>
                             </form>
 
+                            <!-- Update Button -->
                             <div class="py-6 text-center">
                                 <button onclick="submitForm()" class="px-6 py-2 text-white bg-blue-600 rounded-lg">
                                     Update Agent
