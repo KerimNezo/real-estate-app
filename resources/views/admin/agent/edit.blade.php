cba47abc8c4e343c763f3d8c1131bcec12914a7a
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-14 15:00:50 +0100

    Fixed the title of edit Agent page, and removed form tag with other livewire components and added the livewire edit-agent component to the page
    as the summary says

--- resources/views/admin/agent/edit.blade.php
+++ resources/views/admin/agent/edit.blade.php
@@ -1,6 +1,6 @@
 <x-admin-layout>
     <x-slot:title>
-        All properties
+        Update {{ $agent->name }}
     </x-slot:title>
 
     <!-- Page content -->
@@ -25,30 +25,7 @@
                         </h1>
 
                         <div class="w-full h-full px-4">
-                            <form id="updateForm" class="hidden" enctype="multipart/form-data" action="{{ route('update-agent', ['user' => $agent]) }}" method="POST">
-                                @method('PUT')
-                                @csrf
-                                <!-- Table to display agent data-->
-                                <div class="flex flex-col w-full gap-4 lg:flex-row">
-                                    <!-- General property data -->
-                                    <div class="w-full sm:w-[40%] pt-[10px]">
-                                        <livewire:admin.agent-form :$agent />
-                                    </div>
-
-                                    <!-- Agent profile picture section (will be a livewire component) -->
-                                    <div class="w-full sm:w-[60%]">
-                                        <livewire:admin.agent-picture :$agentPicture />
-                                    </div>
-                                </div>
-                            </form>
-
-                            <!-- Update Button -->
-                            <div class="py-6 text-center">
-                                <button onclick="submitForm()" class="px-6 py-2 text-white bg-blue-600 rounded-lg">
-                                    Update Agent
-                                </button>
-                            </div>
-
+                            <livewire:admin.edit-agent :$agent :$agentPicture/>
                         </div>
                     </div>
                 </div>
