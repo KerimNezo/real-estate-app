82bcdf80b586fbca82fd009fe9203c0d8f912ca8
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-15 14:51:57 +0100

    Added if directive to formInput blade to assign error message on $title but if title === password_confirmation then we'll assign it error of password.
    as the summary says

--- resources/views/components/formInput.blade.php
+++ resources/views/components/formInput.blade.php
@@ -2,7 +2,12 @@
 
 <div class="mb-2 mr-auto">
     <label for="{{ $title }}" class="block mb-2 font-bold">{{ $label }}:</label>
-    <input type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg @error($title)
-        border-red-500
-    @enderror" wire:model.blur="{{ $model }}">
+
+    @if ($title === 'password_confirmation')
+        <input type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" wire:model.blur="{{ $model }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg
+        @error('password')  border-red-500 @enderror">
+    @else
+        <input type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" wire:model.blur="{{ $model }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg
+        @error($title)  border-red-500 @enderror">
+    @endif
 </div>
