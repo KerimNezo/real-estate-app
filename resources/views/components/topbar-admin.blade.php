b7ca475489870cae98b225a51ac7c930d4ccb163
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-18 15:26:35 +0100

    Removed pop up messages from topbar that display on admin index pages after a crud Action is finished
    as the summary says. Moved these messages to respective index blades. It
    is kinda dumb, but will just have to make it work.

--- resources/views/components/topbar-admin.blade.php
+++ resources/views/components/topbar-admin.blade.php
@@ -12,27 +12,6 @@
             </button>
         </div>
 
-        <!-- Pop up message displayed to admin after CRUD actions -->
-        @if (session('success'))
-            <div id="sessionMessage" class="relative p-4 text-white bg-green-500 rounded-md w-[400px] justify-center items-center" >
-                <span class="absolute flex items-center justify-center px-[9px] py-0 text-base text-white bg-green-700 rounded-full cursor-pointer top-1 right-1 text-center" onclick="closeSessionMessage()">&times;</span>
-
-                <p class="text-center">
-                    {{ session('success') }}
-                </p>
-            </div>
-        @endif
-
-        @if (session('error'))
-            <div id="sessionMessage" class="relative p-4 text-white bg-red-500 rounded-md w-[400px] justify-center items-center" >
-                <span class="absolute flex items-center justify-center px-[10px] py-0 text-base text-white bg-red-700 rounded-full cursor-pointer top-1 right-1 text-center" onclick="closeSessionMessage()">&times;</span>
-
-                <p class="text-center">
-                    {{ session('error') }}
-                </p>
-            </div>
-        @endif
-
         <div class="pr-4 ml-auto font-bold">
             <p>{{ ucwords(Auth::user()->name) }}</p>
         </div>
@@ -97,21 +76,5 @@
                 }, 300);
             }
         });
-
-        function closeSessionMessage() {
-            document.getElementById('sessionMessage').classList.add('hidden');
-        }
-
-        // function that will automatically close the popup message notifiying the admin about the status of his action
-        window.onload = function() {
-
-            sessionMessage = document.getElementById('sessionMessage');
-
-            if(sessionMessage){
-                setTimeout(() => {
-                    sessionMessage.classList.add('hidden'); // Hide the message
-                }, 7000); // 10000 milliseconds = 10 seconds
-            }
-        };
     </script>
 </div>
