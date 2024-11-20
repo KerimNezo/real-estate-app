95be2050e5e3b124f9217fcabfb0a92fa3aad55e
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-20 16:21:02 +0100

    Started doing create agent livewire component
    as the summary says

--- resources/views/livewire/admin/store-agent.blade.php
+++ resources/views/livewire/admin/store-agent.blade.php
@@ -31,9 +31,43 @@
             </div>
 
             <!-- Agent profile picture section (will be a livewire component) -->
-            {{-- <div class="w-full sm:w-[60%]">
-                <livewire:admin.agent-picture wire:model="novaSlika"/>
-            </div> --}}
+            <div class="w-full sm:w-[60%]">
+                <div class="flex flex-col items-center justify-center w-full">
+                    <!-- Photo gallery header with buttons -->
+                    <div class="flex flex-col items-center justify-center w-full pb-4 sm:flex-row">
+                        <p class="mr-auto text-lg font-bold text-left whitespace-nowrap">
+                            Profile picture:
+                        </p>
+
+                        <div class="flex items-center justify-center w-full gap-4 pt-4 mr-0 sm:pt-0">
+                            <button type="button" onclick="document.getElementById('file-upload').click()" class="px-3 py-2 ml-0 mr-auto text-base text-white bg-green-600 rounded-lg sm:ml-auto sm:mr-0">
+                                <p>New photo</p>
+                            </button>
+
+                            <input type="file" id="file-upload" class="hidden" wire:model="newPhoto" />
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col items-center justify-center w-full py-8">
+                        <div class="relative">
+                            @if ($this->novaSlika !== null && $this->novaSlika->getMimeType() !== '')
+                                <img src="{{ $novaSlika->temporaryUrl() }}" name="newPhoto" alt="Agent's new profile picture" class="bg-gray-800 border-4 border-gray-900 sm:h-72 h-52">
+                            @else
+                                <img src="{{ asset('photos/icons/realestateagent.png') }}" alt="Agent's profile picture" class="bg-gray-800 border-4 border-gray-900 sm:h-72 h-52">
+                            @endif
+
+                            <!-- Loader Overlay, shown while newPhoto is being updated or loading -->
+                            <div wire:loading wire:target="newPhoto" class="absolute inset-0 z-10 flex items-center justify-center bg-gray-700 bg-opacity-75 px-auto">
+                                <img src="{{ asset('photos/spinner.svg') }}" alt="Loading" class="w-full sm:h-72 h-52 opacity-80 pl-auto pr-auto">
+                            </div>
+                        </div>
+
+                        @error('newPhoto')
+                            <p id="error-message" class="pt-4 text-sm text-red-500">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!-- Update Button -->
