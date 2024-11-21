416de359704abe143259acf0f09cf01986d8469d
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-21 15:01:23 +0100

    Added placeholder prop to formInput component that I will need for the create agent component
    as the summary says

--- resources/views/components/formInput.blade.php
+++ resources/views/components/formInput.blade.php
@@ -1,14 +1,14 @@
-@props(['title', 'label', 'model' => '', 'type'])
+@props(['title', 'label', 'model' => '', 'type', 'placeholder' => ''])
 
 <div class="mb-2 mr-auto">
     <label for="{{ $title }}" class="block mb-1 font-bold">{{ $label }}:</label>
 
     <div class="relative">
         @if ($title === 'password_confirmation')
-            <input type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" wire:model.blur="{{ $model }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg
+            <input placeholder="{{ $placeholder }}" type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" wire:model.blur="{{ $model }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg
             @error('password')  border-red-500 @enderror">
         @else
-            <input type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" wire:model.blur="{{ $model }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg
+            <input placeholder="{{ $placeholder }}" type="{{ $type }}" name="{{ $title }}" id="{{ $title }}" wire:model.blur="{{ $model }}" class="w-full px-3 py-2 bg-gray-800 border rounded-lg
             @error($title)  border-red-500 @enderror">
         @endif
 
