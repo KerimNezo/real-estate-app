75be140808b32ddd552c89c6309ac27312c9ebb4
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-27 13:01:14 +0100

    Added checkbox component for the property create blade
    as the summary says

---
+++ resources/views/components/checkbox-input.blade.php
@@ -0,0 +1,11 @@
+@props(['name', 'label'])
+
+<div class="flex items-center w-full mb-4 mr-auto">
+    <div class="flex items-center justify-center mb-2 ml-auto">
+        <input wire:model="$propertyData.{{ $name }}" type="checkbox" name="{{ $name }}" value="1" class="mr-2 font-bold">
+        <p class="font-bold">{{ $label }}</p>
+    </div>
+    @error('$propertyData.{{ $name }}')
+        <p class="text-sm text-red-500">{{ $message }}</p>
+    @enderror
+</div>
