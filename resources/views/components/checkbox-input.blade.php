05aefa3e99359ba76752b1a451977b49b807b274
Kerim Nezo <kerim.nezo@gmail.com>
2024-11-28 13:45:39 +0100

    edited checkbox ml to mr
    as the summary says. re positioned them

--- resources/views/components/checkbox-input.blade.php
+++ resources/views/components/checkbox-input.blade.php
@@ -1,7 +1,7 @@
 @props(['name', 'label'])
 
 <div class="flex items-center w-full mb-4 mr-auto">
-    <div class="flex items-center justify-center mb-2 ml-auto">
+    <div class="flex items-center justify-center mb-2 mr-auto">
         <input wire:model="$propertyData.{{ $name }}" type="checkbox" name="{{ $name }}" value="1" class="mr-2 font-bold">
         <p class="font-bold">{{ $label }}</p>
     </div>
