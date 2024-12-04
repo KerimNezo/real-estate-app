10687c59fca51137ecafd7609f6e1f4ad99f9cf0
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-04 13:37:17 +0100

    Fixed the issue with checkbox-input not storing values
    I never updated wire:model values here from when I still used props as
    members of a single list. Removed that, works like a charm.

--- resources/views/components/checkbox-input.blade.php
+++ resources/views/components/checkbox-input.blade.php
@@ -2,10 +2,10 @@
 
 <div class="flex items-center w-full mb-4 mr-auto">
     <div class="flex items-center justify-center mb-2 mr-auto">
-        <input wire:model="$propertyData.{{ $name }}" type="checkbox" name="{{ $name }}" value="1" class="mr-2 font-bold">
+        <input wire:model="{{ $name }}" type="checkbox" name="{{ $name }}" value="1" class="mr-2 font-bold">
         <p class="font-bold">{{ $label }}</p>
     </div>
-    @error('$propertyData.{{ $name }}')
+    @error('{{ $name }}')
         <p class="text-sm text-red-500">{{ $message }}</p>
     @enderror
 </div>
