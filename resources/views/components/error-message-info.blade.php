7ff5edbc8d04c8691eebb85deabc3fb70c341f7e
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-05 13:57:48 +0100

    Added z index to error-message so it goes over the map

--- resources/views/components/error-message-info.blade.php
+++ resources/views/components/error-message-info.blade.php
@@ -2,7 +2,7 @@
 
 @if ($title === 'null')
     @error($titleVar)
-    <div class="absolute text-red-500 -translate-y-1/2 top-1/2 right-4">
+    <div class="absolute text-red-500 -translate-y-1/2 top-1/2 right-4 z-[4]">
         <div class="relative">
             <!-- The interactive button -->
             <div class="flex items-center justify-center w-6 h-6 border-2 border-red-500 rounded-full cursor-pointer peer">
@@ -11,8 +11,7 @@
 
             <!-- Tooltip -->
             <div
-                class="absolute right-0 hidden px-2 py-1 mb-2 text-sm text-white bg-gray-900 border-gray-900 rounded-lg opacity-75 w-60 border-3 bottom-full
-                peer-hover:block peer-focus:block peer-active:block">
+                class="absolute right-0 hidden px-2 py-1 mb-2 text-sm text-white bg-gray-900 border-gray-900 rounded-lg opacity-75 w-60 border-3 bottom-full peer-hover:block peer-focus:block peer-active:block">
                 {{ $message }}
             </div>
         </div>
@@ -20,7 +19,7 @@
     @enderror
 @else
     @error($title)
-    <div class="absolute text-red-500 -translate-y-1/2 top-1/2 right-4">
+    <div class="absolute text-red-500 -translate-y-1/2 top-1/2 right-4 z-[4]">
         <div class="relative">
             <!-- The interactive button -->
             <div class="flex items-center justify-center w-6 h-6 border-2 border-red-500 rounded-full cursor-pointer peer">
@@ -29,8 +28,7 @@
 
             <!-- Tooltip -->
             <div
-                class="absolute right-0 hidden px-2 py-1 mb-2 text-sm text-white bg-gray-900 border-gray-900 rounded-lg opacity-75 w-60 border-3 bottom-full
-                peer-hover:block peer-focus:block peer-active:block">
+                class="absolute right-0 hidden px-2 py-1 mb-2 text-sm text-white bg-gray-900 border-gray-900 rounded-lg opacity-75 w-60 border-3 bottom-full peer-hover:block peer-focus:block peer-active:block">
                 {{ $message }}
             </div>
         </div>
