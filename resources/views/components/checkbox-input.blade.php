8ae72e761d66a619615da73b15011615f1b2bd42
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-05 14:53:45 +0100

    Made checkboxes fully rounded

--- resources/views/components/checkbox-input.blade.php
+++ resources/views/components/checkbox-input.blade.php
@@ -2,7 +2,7 @@
 
 <div class="flex items-center w-full mb-4 mr-auto">
     <div class="flex items-center justify-center mb-2 mr-auto">
-        <input wire:model="{{ $name }}" type="checkbox" name="{{ $name }}" value="1" class="mr-2 font-bold">
+        <input wire:model="{{ $name }}" type="checkbox" name="{{ $name }}" value="1" class="mr-2 font-bold rounded-full">
         <p class="font-bold">{{ $label }}</p>
     </div>
     @error('{{ $name }}')
