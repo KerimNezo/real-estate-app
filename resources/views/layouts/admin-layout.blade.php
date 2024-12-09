8989bac05b9b98d88894393333618f3cf4decf84
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-09 15:06:14 +0100

    Edited charist link/script in layout

--- resources/views/layouts/admin-layout.blade.php
+++ resources/views/layouts/admin-layout.blade.php
@@ -17,8 +17,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- ChartistJS -->
-        <link rel="stylesheet"
-              href="https://cdn.jsdelivr.net/npm/chartist/dist/chartist.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/chartist/dist/chartist.min.css">
         <script src="https://cdn.jsdelivr.net/npm/chartist/dist/chartist.min.js"></script>
 
         <!-- Leafletjs map -->
