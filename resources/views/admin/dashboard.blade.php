ac4d6c7aeec0492fec1dd18dfd7a43a7f7a7dfae
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-09 15:06:47 +0100

    Started working on admin dashboard that I will introduce to the admin side of the project
    just sayin words. as the summary says

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-admin-layout>
     <x-slot:title>
-        Admin Panel
+        Create agent
     </x-slot:title>
 
     <!-- Page content -->
@@ -10,43 +10,67 @@
             @include('components.topbar-admin')
         </div>
 
-        <div class="flex w-full pt-[150px]">
+        <div class="flex w-full pt-[100px] flex-grow">
             @include('components.sidebar-admin')
 
-            <!-- Main content-->
-            <div id="main-content" class="flex flex-col flex-grow w-full bg-gray-900">
-                <div class="flex flex-col items-center justify-center w-full z-9">
-                    <p class="mb-auto">Welcome back to dashboard page, {{ Auth::user()->name }}</p>
+            <!-- Main content -->
+            <div id="main-content" class="w-full py-6 bg-gray-900">
+                <!-- Update Property Form -->
+                <div class="flex flex-col items-center justify-center p-4 mx-auto bg-gray-900 rounded-lg w-[90%]">
+                    <!-- Title -->
+                    <div class="flex w-full gap-4">
+                        <div class="flex flex-col w-1/2">
+                            <p class="p-1 text-sm">Total profit</p>
 
-                    <!-- Chart container -->
-                    {{-- Responsive design will be a pain.... --}}
-                    <div class="w-[850px] bg-gray-800 rounded-[10px]">
-                        <div class="ct-chart h-[400px] w-full mb-auto pt-4"></div>
+                            <div class="w-full bg-gray-800 rounded-[10px]">
+                                <div class="ct-chart h-[300px] w-full mb-auto pt-4"></div>
+                            </div>
+                        </div>
+
+                        <div class="flex flex-col w-1/2">
+                            <p class="p-1 text-sm">Total profit</p>
+
+                            <div class="w-full bg-gray-800 rounded-[10px]">
+                                <div class="ct-chart1 h-[300px] w-full mb-auto pt-4"></div>
+                            </div>
+                        </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
 
-        <div class="mt-auto">
-            @include('admin.footer')
-        </div>
-
+        @include('admin.footer')
     </div>
+
 
     <script>
         new Chartist.Line('.ct-chart', {
             // labels su kolone, x-osa
-            labels: [1, 2, 3, 4, 5, 6, 7, 8],
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
             // series su redovi, y-osa
             series: [
-                [5, 9, 7, 8, 5, 3, 5, 4]
+                [5, 9, 7, 8, 5, 3, 5, 4, 1, 6, 1, 1]
             ]
         }, {
             low: 0,
-            showArea: true
+            showArea: true,
         });
 
-        // Trebaš skontati kako uraditi: boje, tačknice, datu i onda mijenjanje vremena
-    </script>
+        new Chartist.Line('.ct-chart1', {
+            // labels su kolone, x-osa
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+            // series su redovi, y-osa
+            series: [
+                [5, 10, 25, 8, 14, 10, 16, 5, 11, 9, 10, 11], // First line
+                [2, 6, 17, 3, 7, 4, 9, 3, 5, 4, 3, 5], // Second line
+                [3, 4, 8, 5, 7, 6, 7, 1, 6, 5, 7, 6]  // second line
+            ]
+        }, {
+            low: 0,
+            showArea: true,
+        });
 
+    </script>
 </x-admin-layout>
