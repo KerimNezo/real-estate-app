a8454a202e79597995fa187e351ad6849a7e1e9a
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-10 23:56:34 +0100

    made component to display color explanation on top of charts
    as the summary says

---
+++ resources/views/components/chart-color-text.blade.php
@@ -0,0 +1,10 @@
+@props(['color', 'text'])
+
+<div class="flex items-center justify-center gap-2 pl-2">
+    <p class="text-sm">
+        {{ $text }}
+    </p>
+
+    <div class="bg-[{!! $color !!}] w-4 h-4 rounded-[5px]">
+    </div>
+</div>
