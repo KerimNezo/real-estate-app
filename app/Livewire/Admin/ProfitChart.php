9c60e0fd0ec2c1b48febed4545599bf031ea71d6
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-12 17:15:27 +0100

    Added properties to ProfitChart controller that will later be switched to computed properties if doing makes sense.
    will see how it turns out.

--- app/Livewire/Admin/ProfitChart.php
+++ app/Livewire/Admin/ProfitChart.php
@@ -9,13 +9,16 @@ class ProfitChart extends Component
     public $timeframe = '3';
     public $data = [];
 
+    public $rentSeries, $sellSeries, $totalSeries, $labels = '';
+
     public function mount() {
         // samo promijeni podatke
+
         // $this->dispatch('updateChart', ['labels' => ['Jan', 'Feb', 'Mar', 'Apr'], 'totalSeries' => [3,5,7,9], 'sellSeries' => [2,3,4,5], 'rentSeries' => [1,2,3,4]]);
     }
 
     public function updateGraph(){
-        // ovdje trebam da dodajem
+        // ovdje trebam da skontam logiku po kojoj ću dobijati datu i to samo onda dispatchat.
 
         $this->dispatch('updateChart', ['labels' => ['Jan', 'Feb', 'Mar', 'Apr'], 'totalSeries' => [3,5,7,9], 'sellSeries' => [2,3,4,5], 'rentSeries' => [1,2,3,4]]);
     }
