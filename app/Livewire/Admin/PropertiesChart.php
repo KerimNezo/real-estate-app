7404d96952fbf543782b3084662348fe0322f3b4
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-13 15:22:21 +0100

    EDIT: Updated dispatch event name to better describe functionality of the event sent.
    as the summary says.

--- app/Livewire/Admin/PropertiesChart.php
+++ app/Livewire/Admin/PropertiesChart.php
@@ -12,13 +12,13 @@ class PropertiesChart extends Component
         // samo promijeni podatke
 
         // pobrini se da ovako šalješ datu da bi radilo sve kako treba
-        $this->dispatch('updatePieChart', ['labels' => ['Houses', 'Apartments', 'Offices'], 'series' => [20, 14, 10]]);
+        $this->dispatch('updateDonutChart', ['labels' => ['Houses', 'Apartments', 'Offices'], 'series' => [20, 14, 10]]);
     }
 
     public function updatePieChart(){
         // ovdje trebam da skontam logiku po kojoj ću dobijati datu i to samo onda dispatchat.
 
-        $this->dispatch('updatePieChart', ['labels' => ['Houses', 'Apartments', 'Offices'], 'series' => [20, 40, 40]]);
+        $this->dispatch('updateDonutChart', ['labels' => ['Houses', 'Apartments', 'Offices'], 'series' => [20, 40, 40]]);
     }
 
     public function render()
