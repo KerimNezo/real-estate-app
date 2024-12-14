9a52b0ef64f2c893a041bf5bc6eabe0d7efc465b
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-14 16:20:23 +0100

    Made controller on profitChart dispatch event on mount of the component
    Same with the propertiesChart I had built, now I just need to make the
    logic to fetch the data from the actions table.

--- app/Livewire/Admin/ProfitChart.php
+++ app/Livewire/Admin/ProfitChart.php
@@ -14,7 +14,12 @@ class ProfitChart extends Component
     public function mount() {
         // samo promijeni podatke
 
-        // $this->dispatch('updateLineChart', ['labels' => ['Jan', 'Feb', 'Mar', 'Apr'], 'totalSeries' => [3,5,7,9], 'sellSeries' => [2,3,4,5], 'rentSeries' => [1,2,3,4]]);
+        // podaci se ovako trebaju slati u eventu
+        $this->dispatch('updateLineChart', ['labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+             'totalSeries' => [5, 10, 25, 8, 14, 10, 16, 5, 11, 9, 10, 11],
+             'sellSeries' => [2, 6, 17, 3, 7, 4, 9, 3, 5, 4, 3, 5],
+             'rentSeries' => [3, 4, 8, 5, 7, 6, 7, 1, 6, 5, 7, 6]
+            ]);
     }
 
     public function updateLineChart(){
