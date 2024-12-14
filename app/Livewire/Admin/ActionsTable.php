c708fe137fe15b511d9bd417af66bc800b281ccb
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-14 16:37:44 +0100

    Added computed property actions() and a refresh action that calls render action.
    Don't know if that is the best way to it, regarding the refresh action,
    because I just want to re-fetch the data, not re-render the whole
    component. Maybe it's not that much of an issue if I re-render the whole
    component because the whole component is relatively simple.

--- app/Livewire/Admin/ActionsTable.php
+++ app/Livewire/Admin/ActionsTable.php
@@ -2,10 +2,23 @@
 
 namespace App\Livewire\Admin;
 
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class ActionsTable extends Component
 {
+
+    #[Computed]
+    public function actions()
+    {
+        // funkcija koja će biti computed property
+    }
+
+    public function refreshActions()
+    {
+        $this->render();
+    }
+
     public function render()
     {
         return view('livewire.admin.actions-table');
