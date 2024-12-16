8c2bad5bdedd1ff28804b6e5805a7af400206086
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-16 15:22:29 +0100

    Added 'action-name' component that is used inside actions table to display different colors based on actions name
    as the summary says

---
+++ resources/views/components/action-name.blade.php
@@ -0,0 +1,44 @@
+@props(['name' => 'action_type'])
+
+@switch($name)
+    @case('created')
+        <p class="px-3 py-2 text-sm font-bold text-left bg-teal-700 rounded-xl">
+            {{ strtoupper($name) }}
+        </p>
+        @break
+
+    @case('sold')
+        <p class="px-3 py-2 text-sm font-bold text-left bg-blue-700 rounded-xl">
+            {{ strtoupper($name) }}
+        </p>
+        @break
+
+    @case('rented')
+        <p class="px-3 py-2 text-sm font-bold text-left bg-blue-700 rounded-xl">
+            {{ strtoupper($name) }}
+        </p>
+        @break
+
+    @case('deleted')
+        <p class="px-3 py-2 text-sm font-bold text-left bg-red-700 rounded-xl">
+            {{ strtoupper($name) }}
+        </p>
+        @break
+
+    @case('edited')
+        <p class="px-3 py-2 text-sm font-bold text-left bg-green-700 rounded-xl">
+            {{ strtoupper($name) }}
+        </p>
+        @break
+
+    @case('removed')
+        <p class="px-3 py-2 text-sm font-bold text-left bg-indigo-700 rounded-xl">
+            {{ strtoupper($name) }}
+        </p>
+        @break
+
+    @default
+        <p class="px-3 py-2 text-sm font-bold text-left bg-gray-800 rounded-xl">
+            {{ strtoupper($name) }}
+        </p>
+@endswitch
