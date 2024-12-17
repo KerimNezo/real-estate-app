1dccacd5765e2f867edec4c462b53d4c71aad95b
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-17 15:35:00 +0100

    Added softdeletes to user model as well and made it so that when user is deleted, his properties (if that is to happen) will soft delete
    as the summary says

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\MediaLibrary\HasMedia;
@@ -12,7 +13,20 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements HasMedia
 {
-    use HasFactory, HasRoles, InteractsWithMedia, Notifiable;
+    use HasFactory, HasRoles, InteractsWithMedia, Notifiable, SoftDeletes;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($user) {
+            if ($user->isForceDeleting()) {
+                $user->properties()->forceDelete();
+            } else {
+                $user->properties()->delete();
+            }
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
