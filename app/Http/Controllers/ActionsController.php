6e43a74dcb24583b86bf579d394f6e8b33af48a3
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-17 13:55:51 +0100

    Added show and index controller functions in my ActionController.
    as the summary says. I need to work on my validation.

--- app/Http/Controllers/ActionsController.php
+++ app/Http/Controllers/ActionsController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Actions;
+use Illuminate\Support\Facades\Auth;
 
 class ActionsController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.action.index');
+    }
+
+    public function show(string $id)
+    {
+        return view('admin.action.index', [
+            'actionId' => $id,
+        ]);
+    }
 }
