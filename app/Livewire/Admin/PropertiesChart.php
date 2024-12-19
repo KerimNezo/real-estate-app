053f6f5988d59b885d481c47b00c7c1d22020041
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-19 16:00:19 +0100

    refactored the propertiesChart to have updateChartData($status) that will take in status by which we want to filter properties.
    and added function that triggers on select tags change from previous
    commit

--- app/Livewire/Admin/PropertiesChart.php
+++ app/Livewire/Admin/PropertiesChart.php
@@ -2,23 +2,47 @@
 
 namespace App\Livewire\Admin;
 
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use App\Models\Property;
 
 class PropertiesChart extends Component
 {
-    public $status = "Available";
-
     public function mount() {
-        // samo promijeni podatke
-
-        // pobrini se da ovako šalješ datu da bi radilo sve kako treba
-        $this->dispatch('updateDonutChart', ['labels' => ['Houses', 'Apartments', 'Offices'], 'series' => [20, 14, 10]]);
+        $this->updateChartData('Available');
     }
 
-    public function updatePieChart(){
-        // ovdje trebam da skontam logiku po kojoj ću dobijati datu i to samo onda dispatchat.
+    public function updatePieChart($status){
+        $this->updateChartData($status);
+    }
 
-        $this->dispatch('updateDonutChart', ['labels' => ['Houses', 'Apartments', 'Offices'], 'series' => [20, 40, 40]]);
+    public function updateChartData($status) {
+        $labels = [];
+
+        $series = [];
+
+        $data = Property::query()
+            ->withTrashed()
+            ->select('type_id', DB::raw('count(*) as total'))
+            ->where('status', $status)
+            ->groupBy('type_id')
+            ->get();
+
+        foreach ($data as $property)
+        {
+            if($property->type_id === 1) {
+                $series[] = $property->total;
+                $labels[] = 'Offices';
+            } elseif ($property->type_id === 2) {
+                $series[] = $property->total;
+                $labels[] = 'Houses';
+            } elseif ($property->type_id === 3) {
+                $series[] = $property->total;
+                $labels[] = 'Apartments';
+            }
+        }
+
+        $this->dispatch('updateDonutChart', ['labels' => $labels, 'series' => $series]);
     }
 
     public function render()
