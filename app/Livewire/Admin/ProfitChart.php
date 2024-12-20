54ccd17f30ff3526a280d7d7811bcaab874a7034
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-20 11:20:31 +0100

    Removed hardcoded data and comments from the ProfitChart component controller.
    Now only problem is to write seeder to fill data with each months.

--- app/Livewire/Admin/ProfitChart.php
+++ app/Livewire/Admin/ProfitChart.php
@@ -10,27 +10,16 @@ use Illuminate\Support\Facades\DB;
 class ProfitChart extends Component
 {
     public function mount() {
-        // samo promijeni podatke
 
-        // $this->updateChartData(3)
-
-        // podaci se ovako trebaju slati u eventu
-        $this->dispatch('updateLineChart', ['labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
-             'totalSeries' => [5, 10, 25, 8, 14, 10, 16, 5, 11, 9, 10, 11],
-             'sellSeries' => [2, 6, 17, 3, 7, 4, 9, 3, 5, 4, 3, 5],
-             'rentSeries' => [3, 4, 8, 5, 7, 6, 7, 1, 6, 5, 7, 6]
-            ]);
+        $this->updateChartData(3);
     }
 
     public function updateLineChart($numMonths) {
 
-        // $this->updateChartData($numMonths)
-        // ovdje trebam da skontam logiku po kojoj ću dobijati datu i to samo onda dispatchat.
-
-        $this->dispatch('updateLineChart', ['labels' => ['Jan', 'Feb', 'Mar', 'Apr'], 'totalSeries' => [3,5,7,9], 'sellSeries' => [2,3,4,5], 'rentSeries' => [1,2,3,4]]);
+        $this->updateChartData($numMonths);
     }
 
-    public function updateChartData($months = 3) {
+    public function updateChartData($months) {
 
         // Calculate the starting date (last X months from today)
         $startDate = Carbon::now()->subMonths($months)->startOfMonth();
@@ -75,13 +64,10 @@ class ProfitChart extends Component
 
         logger($chartData);
 
-        // sredi podatke i skontaj kako da ih pošalješ da valja i radit ce.
-
-        // $this->dispatch('updateLineChart', ['labels' => ['Jan', 'Feb', 'Mar', 'Apr'], 'totalSeries' => [3,5,7,9], 'sellSeries' => [2,3,4,5], 'rentSeries' => [1,2,3,4]]);
+        $this->dispatch('updateLineChart', ['labels' => $chartData['labels'], 'totalSeries' => $chartData['total'], 'sellSeries' => $chartData['sold'], 'rentSeries' => $chartData['rented']]);
     }
 
-    public function render()
-    {
+    public function render() {
         return view('livewire.admin.profit-chart');
     }
 }
