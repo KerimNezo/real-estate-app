d7093d2de64cd451263aa4d3043c450c24bf133c
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-22 21:55:24 +0100

    Started working on actions index blade.
    as the summary says

---
+++ resources/views/admin/action/index.blade.php
@@ -0,0 +1,30 @@
+<x-admin-layout>
+    <x-slot:title>
+        Dashboard
+    </x-slot:title>
+
+    <!-- Page content -->
+    <div class="flex flex-col min-h-screen bg-gray-900">
+        <!-- Admin topbar -->
+        <div class="fixed top-0 left-0 z-10 w-full bg-gray-800">
+            @include('components.topbar-admin')
+        </div>
+
+        <div class="flex w-full pt-[100px] flex-grow">
+            @include('components.sidebar-admin')
+
+            <!-- Main content -->
+            <div id="main-content" class="w-full py-6 bg-gray-900">
+                <!-- Admin dashboard page content -->
+                <div class="flex flex-col items-center justify-center mx-auto bg-gray-900 rounded-lg w-[90%]">
+                    <!-- Table display of most recent actions -->
+                    <div class="w-full">
+                        <livewire:admin.actions-index />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        @include('admin.footer')
+    </div>
+</x-admin-layout>
