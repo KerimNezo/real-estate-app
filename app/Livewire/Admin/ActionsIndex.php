cfe18ba9f394cb0490e13a945984b6f688f0753b
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-22 21:57:56 +0100

    Started working on actions index controller. Need more editing on form and query builder.
    as the summary says

---
+++ app/Livewire/Admin/ActionsIndex.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Actions;
+use App\Models\User;
+use Livewire\Attributes\Computed;
+
+class ActionsIndex extends Component
+{
+    use WithPagination;
+
+    #[Computed]
+    public function agents()
+    {
+        return User::query()
+            ->select('id', 'name')
+            ->get();
+    }
+
+    #[Computed]
+    public function actions()
+    {
+        return Actions::query()
+            ->select(['id', 'property_id', 'user_id', 'name', 'created_at'])
+            ->latest()
+            ->with(['user','property'])
+            ->paginate(10);
+    }
+
+    public function submitForm()
+    {
+        logger('Form submitted');
+        // this action here will just change the values of component properties, and when those change
+        // by default the computed property will update.
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.actions-index');
+    }
+}
