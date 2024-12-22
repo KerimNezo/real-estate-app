95eed9560a9f386d3b38fff6d292a4fb35be698c
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-22 21:56:46 +0100

    Moved table to different div that is on same level with footer. So the overflow doesn't apply on footer.
    as the summary says

--- resources/views/livewire/admin/actions-table.blade.php
+++ resources/views/livewire/admin/actions-table.blade.php
@@ -8,103 +8,108 @@
         </div>
 
         <!-- Actions Table Content-->
-        <div class="overflow-x-auto" wire:loading.class="opacity-30">
-            <table class="min-w-full overflow-hidden bg-gray-800 rounded-t-xl">
-                <!-- Action Tables Header-->
-                <thead class="bg-gray-800 border-gray-700">
-                    <tr id="table-header" class="border-b border-gray-700">
-                        <!-- Id -->
-                        <x-table.table-header title="Id" />
+        <div wire:loading.class="opacity-30">
 
-                        <!-- Agent -->
-                        <x-table.table-header title="Agent" />
+            <div class="overflow-x-auto" >
+                <table class="min-w-full overflow-hidden bg-gray-800 rounded-t-xl">
+                    <!-- Action Tables Header-->
+                    <thead class="bg-gray-800 border-gray-700">
+                        <tr id="table-header" class="border-b border-gray-700">
+                            <!-- Id -->
+                            <x-table.table-header title="Id" />
 
-                        <!-- Action -->
-                        <x-table.table-header title="Action" />
-
-                        <!-- Property-->
-                        <x-table.table-header title="Property" />
-
-                        <!-- Created -->
-                        <x-table.table-header title="Created" />
-
-                        <!-- Options -->
-                        <x-table.table-header title="Options" />
-
-                        <!-- Refresh Table Button-->
-                        <th class="pr-2">
-                            <button wire:click="refreshActions()" class="p-1 ml-auto bg-gray-700 rounded-xl">
-                                <x-ionicon-refresh-outline class="w-5" />
-                            </button>
-                        </th>
-                    </tr>
-                </thead>
-
-                <!-- Action Tables Content-->
-                <tbody>
-                    @foreach ($this->actions as $action)
-                        <tr class="border-t border-gray-700" wire:key="1">
-                            <!-- Action Id -->
-                            <td class="px-4 py-3">
-                                <div class="flex items-center justify-start">
-                                    <a href="{{ route('dashboard') }}">
-                                        <p class="text-sm text-left hover:text-white">
-                                            {{ $action->id }}
-                                        </p>
-                                    </a>
-                                </div>
-                            </td>
-
-                            <!-- Agent name -->
-                            <td class="px-4 py-3">
-                                <div class="flex items-center justify-start">
-                                    <a href="{{ route('single-agent', ['user' => $action->user])}}">
-                                        <p class="text-sm text-left hover:text-white">
-                                            {{ $action->user->name }}
-                                        </p>
-                                    </a>
-                                </div>
-                            </td>
+                            <!-- Agent -->
+                            <x-table.table-header title="Agent" />
 
                             <!-- Action -->
-                            <td class="px-4 py-3">
-                                <div class="flex items-center justify-start">
-                                    <x-action-name :name="$action->name" />
-                                </div>
-                            </td>
+                            <x-table.table-header title="Action" />
 
-                            <!-- Property Name -->
-                            <td class="px-4 py-3">
-                                <div class="flex items-center sjustify-start">
-                                    <a href="{{ route('admin-single-property', [ 'user' => $action->property->user_id, 'property' => $action->property])}}">
-                                        <p class="text-sm text-left hover:text-white">
-                                            {{ $action->property->name }}
-                                        </p>
-                                    </a>
-                                </div>
-                            </td>
+                            <!-- Property-->
+                            <x-table.table-header title="Property" />
 
-                            <!-- Time -->
-                            <td class="px-4 py-3">
-                                <div class="flex items-center justify-start">
-                                    <p class="text-sm text-left">
-                                        {{ $action->created_at->diffForHumans()}}
-                                    </p>
-                                </div>
-                            </td>
+                            <!-- Created -->
+                            <x-table.table-header title="Created" />
 
-                            <!-- Options-->
-                            <td class="px-4 py-3">
-                                <div class="flex items-center justify-start">
-                                    <a href="{{ route('admin-single-action', ['id' => $action->id]) }}" class="hover:text-red-400">
-                                        <x-carbon-view class="w-[25px]"/>
-                                    </a>
-                                </div>
-                            </td>
+                            <!-- Options -->
+                            <x-table.table-header title="Options" />
+
+                            <!-- Refresh Table Button-->
+                            <th class="pr-2">
+                                <button wire:click="refreshActions()" class="p-1 ml-auto bg-gray-700 rounded-xl">
+                                    <x-ionicon-refresh-outline class="w-5" />
+                                </button>
+                            </th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+
+                    <!-- Action Tables Content-->
+                    <tbody>
+                        @foreach ($this->actions as $action)
+                            <tr class="border-t border-gray-700" wire:key="1">
+                                <!-- Action Id -->
+                                <td class="px-4 py-3">
+                                    <div class="flex items-center justify-start">
+                                        <a href="{{ route('dashboard') }}">
+                                            <p class="text-sm text-left hover:text-white">
+                                                {{ $action->id }}
+                                            </p>
+                                        </a>
+                                    </div>
+                                </td>
+
+                                <!-- Agent name -->
+                                <td class="px-4 py-3">
+                                    <div class="flex items-center justify-start">
+                                        <a href="{{ route('single-agent', ['user' => $action->user])}}">
+                                            <p class="text-sm text-left hover:text-white">
+                                                {{ $action->user->name }}
+                                            </p>
+                                        </a>
+                                    </div>
+                                </td>
+
+                                <!-- Action -->
+                                <td class="px-4 py-3">
+                                    <div class="flex items-center justify-start">
+                                        <x-action-name :name="$action->name" />
+                                    </div>
+                                </td>
+
+                                <!-- Property Name -->
+                                <td class="px-4 py-3">
+                                    <div class="flex items-center sjustify-start">
+                                        <a href="{{ route('admin-single-property', [ 'user' => $action->property->user_id, 'property' => $action->property])}}">
+                                            <p class="text-sm text-left hover:text-white">
+                                                {{ $action->property->name }}
+                                            </p>
+                                        </a>
+                                    </div>
+                                </td>
+
+                                <!-- Time -->
+                                <td class="px-4 py-3">
+                                    <div class="flex items-center justify-start">
+                                        <p class="text-sm text-left">
+                                            {{ $action->created_at->diffForHumans()}}
+                                        </p>
+                                    </div>
+                                </td>
+
+                                <!-- Options-->
+                                <td class="px-4 py-3">
+                                    <div class="flex items-center justify-start">
+                                        <a href="{{ route('admin-single-action', ['id' => $action->id]) }}" class="hover:text-red-400">
+                                            <x-carbon-view class="w-[25px]"/>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            {{-- All actions link --}}
             <div class="flex items-center justify-center w-full p-2 bg-gray-800 border-t border-gray-700 rounded-b-xl">
                 <a href="{{ route('admin-actions') }}" class="mx-auto cursor-pointer">
                     <p class="text-sm font-bold cursor-pointer hover:text-white">
