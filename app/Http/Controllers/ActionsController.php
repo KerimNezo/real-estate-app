7d496173cf4d8b6b65058d2418523749ed15323e
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-23 11:51:48 +0100

    Worked on show action in actionsController.
    Added nothing completely new, just made it so I can display data
    correctly.

--- app/Http/Controllers/ActionsController.php
+++ app/Http/Controllers/ActionsController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Actions;
+use Illuminate\Notifications\Action;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
 
 class ActionsController extends Controller
 {
@@ -15,8 +17,20 @@ class ActionsController extends Controller
 
     public function show(string $id)
     {
+        $action = Actions::query()
+            ->select('id', 'user_id', 'property_id', 'name', 'message', 'created_at')
+            ->with(['user', 'property']) // Adjust relations as needed
+            ->findOrFail($id);
+
+        $actionData = $action->getAttributes();
+
+        unset($actionData['user_id'], $actionData['property_id']);
+
+        $actionData['created_at'] = Carbon::parse($action->created_at)->format('F j, Y \a\t g:i A');
+
         return view('admin.action.show', [
-            'actionId' => $id,
+            'action' => $action,
+            'actionData' => $actionData,
         ]);
     }
 }
