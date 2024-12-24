8d370d1c04cf750d2aa997084953b8209a652990
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-24 13:55:27 +0100

    Started working on property-index in agent views
    as the summary says

--- resources/views/agent/property/index.blade.php
+++ resources/views/agent/property/index.blade.php
@@ -1,1 +1,27 @@
-<p>hello</p>
+<x-admin-layout>
+    <x-slot:title>
+        All properties
+    </x-slot:title>
+
+    <!-- Page content -->
+    <div id="page-content" class="flex flex-col min-h-screen bg-gray-900">
+        <!-- Admin topbar -->
+        <div class="fixed top-0 left-0 z-10 w-full bg-gray-800">
+            @include('components.topbar-admin')
+        </div>
+
+        <div class="flex w-full pt-[100px] flex-grow">
+            @include('components.sidebar-agent')
+
+            <!-- Main content -->
+            <div id="main-content" class="flex flex-col flex-grow w-full pt-6 bg-gray-900">
+                @include('components.admin-session-messages')
+
+                <!-- SearchForm/Table -->
+                <livewire:admin.property-index :$minPrice :$maxPrice :$assetLocation :$assetTypeId :$cities/>
+            </div>
+        </div>
+
+        @include('admin.footer')
+    </div>
+</x-admin-layout>
