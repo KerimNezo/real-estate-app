e1ab381e6e77ddd09eabbfb1edad38be03480ccf
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-24 15:41:58 +0100

    Added agent single/edit/update property routes to agent routes. There is still work to do here
    as the summary says. Keep track of your notes on what is finished and
    what's not.

--- routes/web.php
+++ routes/web.php
@@ -89,11 +89,20 @@ Route::middleware(['role:agent', 'auth', 'verified'])->group(function () {
     Route::get('agent/property/index', [AgentController::class, 'indexProperties'])
         ->name('agent-properties');
 
+    Route::get('agent/property/{property}', [AgentController::class, 'showProperty'])
+        ->name('agent-single-property');
+
     Route::get('agent/property/create', [PropertyController::class, 'create'])
         ->name('agent-new-property');
 
     Route::post('agent/property/create', [])
         ->name('agent-store-property');
+
+    Route::get('agent/property/{property}/edit', [AgentController::class, 'editProperty'])
+        ->name('agent-edit-property');
+
+    Route::put('agent/property/{property}', [PropertyController::class, 'update'])
+        ->name('agent-update-property');
 });
 
 // Not my routes
