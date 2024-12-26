0e1a64e9fa52e41ee425839ea4d2b5ce35f0d4de
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-26 15:18:51 +0100

    Added auth facade to agentPropertyTable livewire component, to query properties with 'Unavailable' status if component is being used in Agent views
    This is needed because this component is used in agent-dashboard to
    display properties that need to be edited and are put to unavailable
    display because they're not ready for sale.

--- app/Livewire/Admin/AgentPropertyTable.php
+++ app/Livewire/Admin/AgentPropertyTable.php
@@ -6,6 +6,7 @@ use App\Models\Property;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Support\Facades\Auth;
 
 class AgentPropertyTable extends Component
 {
@@ -24,6 +25,10 @@ class AgentPropertyTable extends Component
                 $query->orderBy('order_column', 'asc')
                     ->limit(1);
             }]);
+
+        if(Auth::user()->hasRole('agent')) {
+            $prop = $prop->where('status', '=', 'Unavailable');
+        }
 
         return $prop->paginate(5);
     }
