fa64048b24f8726831da699dadc269226278c9fa
Kerim Nezo <kerim.nezo@gmail.com>
2024-12-27 16:43:09 +0100

    Edited way I send data to the front end and how I dispatch the data
    as the summary says

--- app/Livewire/Admin/PropertiesChart.php
+++ app/Livewire/Admin/PropertiesChart.php
@@ -8,21 +8,18 @@ use Livewire\Component;
 
 class PropertiesChart extends Component
 {
+    public $series, $labels;
+
     public function mount()
     {
         $this->updateChartData('Available');
     }
 
-    public function updatePieChart($status)
-    {
-        $this->updateChartData($status);
-    }
-
     public function updateChartData($status)
     {
-        $labels = [];
+        $this->labels = [];
 
-        $series = [];
+        $this->series = [];
 
         $data = Property::query()
             ->withTrashed()
@@ -33,18 +30,18 @@ class PropertiesChart extends Component
 
         foreach ($data as $property) {
             if ($property->type_id === 1) {
-                $series[] = $property->total;
-                $labels[] = 'Offices';
+                $this->series[] = $property->total;
+                $this->labels[] = 'Offices';
             } elseif ($property->type_id === 2) {
-                $series[] = $property->total;
-                $labels[] = 'Houses';
+                $this->series[] = $property->total;
+                $this->labels[] = 'Houses';
             } elseif ($property->type_id === 3) {
-                $series[] = $property->total;
-                $labels[] = 'Apartments';
+                $this->series[] = $property->total;
+                $this->labels[] = 'Apartments';
             }
         }
 
-        $this->dispatch('updateDonutChart', ['labels' => $labels, 'series' => $series]);
+        $this->dispatch('updateDonutChart', ['labels' => $this->labels, 'series' => $this->series]);
     }
 
     public function render()
