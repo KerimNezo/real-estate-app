c478d87c3949aa129d8bd39d6bcabdf74a0b04d7
Kerim Nezo <kerim.nezo@gmail.com>
2025-01-08 14:38:24 +0100

    Made it so WelcomeController doesn't query all available properties on the home page. Just takes 6 latest.
    as the summary says

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,10 +11,12 @@ class WelcomeController extends Controller
         $properties = Property::query()
             ->select('id', 'type_id', 'name', 'price', 'city', 'bedrooms', 'garage', 'furnished', 'floors', 'lease_duration', 'keycard_entry', 'surface', 'toilets')
             ->where('status', '=', 'Available')
+            ->latest()
             ->with(['media' => function ($query) {
                 $query->orderBy('order_column', 'asc')
                     ->limit(1);
             }])
+            ->take(6)
             ->get();
 
         $propertyCount = $properties->count();
