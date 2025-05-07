54f70f85ca3bf313ea1b2b24b12a6cd9d8ae4761
Kerim Nezo <kerim.nezo@gmail.com>
2025-05-07 10:18:39 +0200

    include admin-sessios-message to the action/index file
    as the summary says

--- resources/views/admin/action/index.blade.php
+++ resources/views/admin/action/index.blade.php
@@ -17,6 +17,8 @@
             <div id="main-content" class="w-full py-6 bg-gray-900">
                 <!-- Admin dashboard page content -->
                 <div class="flex flex-col items-center justify-center mx-auto bg-gray-900 rounded-lg w-[90%]">
+                    @include('components.admin-session-messages')
+
                     <!-- Table display of most recent actions -->
                     <div class="w-full">
                         <livewire:admin.actions-index />
